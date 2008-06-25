538c896230d3fd1f81329dd9c814d08ba8531199
andreafrancia <andreafrancia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2008-06-25 22:06:36 +0000

    Improved the failure messages from test.bash Still some test fails.

--- test.bash
+++ test.bash
@@ -8,35 +8,44 @@ testPrintVersion()
 
 testTrashAbsolutePath()
 {
-	export XDG_DATA_HOME="/tmp/temp-dir"
+	export XDG_DATA_HOME="./test-dir"
 	rm -Rfv "$XDG_DATA_HOME/Trash"
 	
-        touch /tmp/dummy
-	./trash /tmp/dummy
-	info_content="$(<"$XDG_DATA_HOME/Trash/info/dummy.trashinfo")"
-        [[ "$info_content" == "[Trash Info]
-Path=/tmp/dummy
-DeletionDate="* ]];
-        assertEquals 0 $?
+        touch dummy
+	./trash dummy
+        
+        dummy_fullpath="$(pwd)"/dummy
+	
+        info_content="$(<"$XDG_DATA_HOME/Trash/info/dummy.trashinfo")"
+
+        info_content_line_1="$(echo "$info_content" | sed -n '1p' )"
+        info_content_line_2="$(echo "$info_content" | sed -n '2p' )"
+        info_content_line_3="$(echo "$info_content" | sed -n '3p' )"
+ 
+        assertEquals "[Trash Info]" "$info_content_line_1"
+        assertEquals "Path=$dummy_fullpath" "$info_content_line_2"
+        assertTrue "DeletionDate problem" '[[ '"$info_content_line_3"' == DeletionDate=????-??-??T??:??:?? ]]'
+
 	rm -Rfv "$XDG_DATA_HOME/Trash"
 }
 
 testTrashRelativePath()
 {
-	export XDG_DATA_HOME="/tmp/temp-dir"
+	export XDG_DATA_HOME="./test-dir"
 	rm -Rfv "$XDG_DATA_HOME/Trash"
 	
-        touch /tmp/temp-dir/dummy
-	./trash /tmp/temp-dir/dummy
+        touch ./test-dir/dummy
+	./trash ./test-dir/dummy
 	info_content="$(<"$XDG_DATA_HOME/Trash/info/dummy.trashinfo")"
+
         info_content_line_1="$(echo "$info_content" | sed -n '1p' )"
         info_content_line_2="$(echo "$info_content" | sed -n '2p' )"
         info_content_line_3="$(echo "$info_content" | sed -n '3p' )"
         
         assertEquals "[Trash Info]" "$info_content_line_1"
         assertEquals "Path=dummy" "$info_content_line_2"
-        
         assertTrue "DeletionDate problem" '[[ '"$info_content_line_3"' == DeletionDate=????-??-??T??:??:?? ]]'
+
 	rm -Rfv "$XDG_DATA_HOME/Trash"
 }
 
