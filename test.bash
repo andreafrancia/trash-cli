9993f127d3297a5d0d568db9356ded47cde2a0b6
andreafrancia <andreafrancia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2008-07-06 01:13:13 +0000

    Added new test procedures.

--- test.bash
+++ test.bash
@@ -1,54 +1,151 @@
 #!/bin/bash
 
+topdir=""
+uid="1000"
+
 testPrintVersion()
 {
 	./trash --version
 	assertEquals 0 "$?"
 }
 
-testTrashAbsolutePath()
-{
-	export XDG_DATA_HOME="./test-dir"
-	rm -Rfv "$XDG_DATA_HOME/Trash"
-	
-        touch dummy
-	./trash dummy
-        
-        dummy_fullpath="$(pwd)"/dummy
-	
-        info_content="$(<"$XDG_DATA_HOME/Trash/info/dummy.trashinfo")"
+# Usage:
+#   check_trashinfo <path-to-trashinfo> <expected-path>
+# Description:
+#   Check that the trashinfo file contains the expected information
+check_trashinfo() {
+    local trashinfo="$1"
+    local expected_path="$2"
 
-        info_content_line_1="$(echo "$info_content" | sed -n '1p' )"
-        info_content_line_2="$(echo "$info_content" | sed -n '2p' )"
-        info_content_line_3="$(echo "$info_content" | sed -n '3p' )"
- 
-        assertEquals "[Trash Info]" "$info_content_line_1"
-        assertEquals "Path=$dummy_fullpath" "$info_content_line_2"
-        assertTrue "DeletionDate problem" '[[ '"$info_content_line_3"' == DeletionDate=????-??-??T??:??:?? ]]'
+    assertEquals 3 "$(wc -l <$trashinfo)"
 
-	rm -Rfv "$XDG_DATA_HOME/Trash"
+    local line1="$(sed -n '1p'< "$trashinfo")"
+    local line2="$(sed -n '2p'< "$trashinfo")"
+    local line3="$(sed -n '3p'< "$trashinfo")"
+
+    assertEquals "[Trash Info]" "[Trash Info]" "$line1"
+    assertEquals "Path" "Path=$expected_path" "$line2"
+    assertTrue "DeletionDate" '[[ '"$line3"' == DeletionDate=????-??-??T??:??:?? ]]'
+
 }
 
-testTrashRelativePath()
-{
-	export XDG_DATA_HOME="./test-dir"
-	rm -Rfv "$XDG_DATA_HOME/Trash"
-	
-        touch ./test-dir/dummy
-	./trash ./test-dir/dummy
-	info_content="$(<"$XDG_DATA_HOME/Trash/info/dummy.trashinfo")"
+# usage:
+#  assert_is_trashed <trashcan> <original-content> <original-path> <expected_trash_name> <expected-path-for-trashinfo>
+assert_is_trashed () {
+    local trashcan="$1"
+    local original_content="$2"
+    local original_path="$3"
+    local expected_trash_name="$4"
+    local expected_trashinfo_path="$5"
 
-        info_content_line_1="$(echo "$info_content" | sed -n '1p' )"
-        info_content_line_2="$(echo "$info_content" | sed -n '2p' )"
-        info_content_line_3="$(echo "$info_content" | sed -n '3p' )"
-        
-        assertEquals "[Trash Info]" "$info_content_line_1"
-        assertEquals "Path=dummy" "$info_content_line_2"
-        assertTrue "DeletionDate problem" '[[ '"$info_content_line_3"' == DeletionDate=????-??-??T??:??:?? ]]'
+    assertTrue "The file has been deleted?" "[ ! -e \"$original_path\" ]"
 
-	rm -Rfv "$XDG_DATA_HOME/Trash"
+    # check that trashcan has been created
+    assertTrue "Trashcan has been created?" "[ -d \"$trashcan\" ]"
+    assertTrue "Trashcan contains files?"   "[ -d \"$trashcan/files\" ]"
+    assertTrue "Trashcan contains info?"    "[ -d \"$trashcan/info\" ]"
+
+    # check that the file has been trashed
+    assertTrue "[ -e \"$trashcan/files/$expected_trash_name\" ]"
+    assertTrue "[ -f \"$trashcan/info/$expected_trash_name.trashinfo\" ]"
+
+    check_trashinfo "$trashcan/info/$expected_trash_name.trashinfo" "$expected_trashinfo_path"
+    
+    assertEquals "$original_content" "$(<$trashcan/files/$expected_trash_name)"
+
 }
 
+# usage:
+#   1. trash_in_home_trashcan <file>
+#   2. trash_in_home_trashcan <dir>
+test_trash_in_home_trashcan() {
+    export XDG_DATA_HOME="./test-dir"
+    rm -Rf "$XDG_DATA_HOME"
+
+    # trash a file when trashcan is not yet created 
+    echo 'Hello World!' > file-to-trash
+    ./trash file-to-trash
+
+    assert_is_trashed $XDG_DATA_HOME/Trash "Hello World!" file-to-trash file-to-trash "$(pwd)/file-to-trash"
+
+    # trash a file when trashcan is yet created 
+    echo 'Hello World 2!' > other-file-to-trash
+    ./trash other-file-to-trash
+
+    assert_is_trashed $XDG_DATA_HOME/Trash "Hello World 2!" other-file-to-trash other-file-to-trash "$(pwd)/file-to-trash"
+
+    # trash a file with the same name of already trashed file
+    echo 'Hello World 3!' > file-to-trash
+    ./trash file-to-trash
+
+    assert_is_trashed $XDG_DATA_HOME/Trash "Hello World 3!" file-to-trash file-to-trash "$(pwd)/file-to-trash"
+
+}
+
+do_test_trash_in_volume_trashcan() {
+	local trashcan="$1"
+
+	# delete trashcan
+	rm -Rf "$trashcan"
+
+    	# trash a file when trashcan is not yet created
+	echo 'Hello World!' > "/tmp/file-to-trash"
+	./trash "/tmp/file-to-trash"
+	assert_is_trashed "$trashcan" "Hello World!" /tmp/file-to-trash file-to-trash tmp/file-to-trash
+
+    	# trash a file when trashcan is yet created 
+    	echo 'Hello World 2!' > "/tmp/other-file-to-trash"
+    	./trash "/tmp/other-file-to-trash"	
+    	assert_is_trashed "$trashcan" "Hello World 2!" /tmp/other-file-to-trash other-file-to-trash tmp/other-file-to-trash
+
+        # trash a file with the same name of already trashed file
+	echo 'Hello World 3!' > "/tmp/file-to-trash"
+	./trash "/tmp/file-to-trash"
+	assert_is_trashed "$trashcan" "Hello World 3!" /tmp/file-to-trash file-to-trash_1 tmp/file-to-trash
+}
+
+
+test_trash_in_volume_trashcans_when_Trash_does_not_exist() {
+	rm -Rf $topdir/.Trash
+	do_test_trash_in_volume_trashcan "$topdir/.Trash-$uid"
+}
+
+test_trash_in_volume_trashcans_when_Trash_is_not_sticky_nor_writable() {
+	rm -Rf $topdir/.Trash
+	mkdir --parent $topdir/.Trash
+	chmod a-t $topdir/.Trash	
+	chmod a-w $topdir/.Trash
+	do_test_trash_in_volume_trashcan "$topdir/.Trash-$uid"
+}
+
+test_trash_in_volume_trashcans_when_Trash_is_not_sticky() {
+	rm -Rf $topdir/.Trash
+	mkdir --parent $topdir/.Trash
+	chmod a-t $topdir/.Trash	
+	chmod a+w $topdir/.Trash
+	do_test_trash_in_volume_trashcan "$topdir/.Trash-$uid"
+}
+
+test_trash_in_volume_trashcans_when_Trash_is_not_writable() {
+	rm -Rf $topdir/.Trash
+	mkdir --parent $topdir/.Trash
+	chmod a+t $topdir/.Trash	
+	chmod a-w $topdir/.Trash
+	do_test_trash_in_volume_trashcan "$topdir/.Trash-$uid"
+}
+
+test_trash_in_volume_trashcans_when_Trash_is_ok() {
+	rm -Rf $topdir/.Trash
+	mkdir --parent $topdir/.Trash
+	chmod u+t $topdir/.Trash
+	chmod a+w $topdir/.Trash
+	do_test_trash_in_volume_trashcan "$topdir/.Trash/$uid"
+}
+
+if [  -e $topdir/.Trash ]; then
+	echo "To run these test you should remove $topdir/.Trash first."
+	exit
+fi
 
 # load shunit2
 . test-lib/shunit2/src/shell/shunit2
