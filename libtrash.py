bcffa088b5b18faa124182c83488a9d77e998ae3
andreafrancia <andreafrancia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2008-07-07 18:16:15 +0000

    Added many unit tests.

--- libtrash.py
+++ libtrash.py
@@ -1,9 +1,19 @@
-import os 
+import StringIO
+import exceptions
+import os
+import re
 import shutil
+import string
+import sys
 import time
+import urllib
+import random
+from datetime import datetime
+from stat import *
 
+
+# the distribution script will change this to actual version number
 version='svn'
-
 
 class File (object) :
     def __init__(self, path) :
@@ -12,8 +22,8 @@ class File (object) :
     def getParent(self) :
         return File(os.path.dirname(self.path))
     
-    def getRealParent(self) :
-        return File(os.path.realpath(os.path.dirname(self.path)))
+    def getRealPath(self) :
+        return File(os.path.realpath(self.path))
     
     def getBasename(self) :
         return os.path.basename(self.path)
@@ -43,7 +53,8 @@ class File (object) :
     """
     return Volume the volume where the file is
     """
-    def getVolume(self) :
+    @property
+    def volume(self) :
         return Volume.volumeOf(self.getPath())
 
     def getPath(self) :
@@ -63,23 +74,18 @@ class File (object) :
 
     def islink(self) :
         return os.path.islink(self.getPath())
-
-import os
-import random
-from datetime import datetime
-
-try:
-    mygetuid = os.getuid
-except AttributeError:
-    mygetuid = lambda: 1234
-
+    
+    
 """
 Represent a trash directory.
 For example $XDG_DATA_HOME/Trash
 """
-class TrashDirectory(File) :
-    def __init__(self, path) :
-        File.__init__(self,path)
+class TrashDirectory(object) :
+    def __init__(self, path, volume) :
+        assert isinstance(path,File)
+        assert isinstance(volume,File)
+        self.path = path
+        self.volume = volume
 
     """ 
     Trash the specified file.
@@ -87,7 +93,7 @@ class TrashDirectory(File) :
     """
     def trash(self, fileToBeTrashed):
         assert(isinstance(fileToBeTrashed, File))
-        if not self.getVolume() == fileToBeTrashed.getParent().getVolume() :
+        if not self.volume == fileToBeTrashed.getParent().volume :
             raise "file is not in the same volume of trash directory!"
 
         trashInfo = self.createTrashInfo(fileToBeTrashed, datetime.now())
@@ -104,13 +110,13 @@ class TrashDirectory(File) :
         return TrashedFile(trashInfo, self)
 
     def getBasePath(self) :
-        return self.getVolume().getPath()
+        return self.volume.getPath()
 
     def getInfoPath(self) :
-        return os.path.join(self.getPath(), "info")
+        return os.path.join(self.path.path, "info")
 
     def getFilesPath(self) :
-        return os.path.join(self.getPath(), "files")
+        return os.path.join(self.path.path, "files")
             
     def trashedFiles(self) :
         try : 
@@ -147,7 +153,11 @@ class TrashDirectory(File) :
 
     def removeInfoFile(self, trashId) :
         self.getTrashInfoFile(trashId).remove()
+        
+    def _path_for_trashinfo(self, fileToTrash):
+        raise NotImplementedError
 
+    
     """
     Create a .trashinfo file in the $trash/info directory.
     param File fileToBeTrashed
@@ -156,20 +166,10 @@ class TrashDirectory(File) :
     """
     def createTrashInfo(self, fileToBeTrashed, deletionTime) :
         assert(isinstance(fileToBeTrashed, File))
-        # calculate relative path
-        parent = fileToBeTrashed.getRealParent()
-        volume = self.getVolume()
 
-        if parent.getPath().startswith(volume.getPath() + os.sep) :
-            relativeParentPath = parent.getPath()[len(volume.getPath())+len(os.sep):]
-        else :
-            relativeParentPath = parent.getPath()
-        
-        relativePath = os.path.join(relativeParentPath, fileToBeTrashed.getBasename())
-        
         # create trash info
         trashInfo = TrashInfo()
-        trashInfo.setPath(relativePath)
+        trashInfo.setPath(self._path_for_trashinfo(fileToBeTrashed))
         trashInfo.setDeletionTime(deletionTime)
         
         # write trash info
@@ -202,7 +202,6 @@ class TrashDirectory(File) :
             index += 1
             
         raise IOError()
-        
     
     # staticmethod
     def getHomeTrashDirectory() : 
@@ -211,8 +210,8 @@ class TrashDirectory(File) :
         else :
             XDG_DATA_HOME = os.environ['HOME'] + '/.local/share'
             
-        path = XDG_DATA_HOME + "/Trash"
-        return TrashDirectory(path)
+        path = File(XDG_DATA_HOME + "/Trash")
+        return HomeTrashDirectory(path)
     getHomeTrashDirectory=staticmethod(getHomeTrashDirectory)
 
     # staticmethod
@@ -234,9 +233,41 @@ class TrashDirectory(File) :
             if trashedfile.getPath().startswith(dir + os.path.sep) :
                 yield trashedfile
     allTrashedFilesInDir=classmethod(allTrashedFilesInDir)    
+    
+class HomeTrashDirectory(TrashDirectory) :
+    def __init__(self, path) :
+        assert isinstance(path, File)
 
-import os
+        TrashDirectory.__init__(self, path, path.volume)
+                       
+    def _path_for_trashinfo(self, fileToBeTrashed) :
+        assert isinstance(fileToBeTrashed, File)
+        
+        # for the HomeTrashDirectory all path are stored as absolute
+        
+        parent = fileToBeTrashed.getParent().getRealPath().getPath()        
+        return os.path.join(parent, fileToBeTrashed.getBasename())
 
+class VolumeTrashDirectory(TrashDirectory) :
+    def __init__(self, path, volume) :
+        assert isinstance(path, File)
+        assert isinstance(volume, File)
+        TrashDirectory.__init__(self,path, volume)
+        
+    def _path_for_trashinfo(self, fileToBeTrashed) :
+        # for the VolumeTrashDirectory paths are stored as relative 
+        # if possible
+        
+        # string representing the parent of the fileToBeTrashed
+        parent = fileToBeTrashed.getParent().getRealPath().getPath()        
+        topdir=self.volume.getPath()   # e.g. /mnt/disk-1
+        
+        if parent.startswith( topdir + os.sep) :
+            parent = parent[len(topdir + os.sep):]
+            
+        return os.path.join(parent, fileToBeTrashed.getBasename())                      
+
+    
 class TrashedFile (object) :
     def __init__(self,trashinfo, trashdirectory) :
         self.__trashinfo = trashinfo
@@ -263,12 +294,6 @@ class TrashedFile (object) :
         self.__trashdirectory.getTrashInfoFile(trashId).remove()
 
 
-import StringIO
-import re
-import exceptions
-import urllib
-from datetime import datetime
-import os
 
 class TrashInfo (object) :
     def __init__(self, trashId = None) :
@@ -336,24 +361,23 @@ class TrashInfo (object) :
         result += "DeletionDate=" + self.getDeletionTimeAsString() + "\n"
         return result
 
-class TimeUtils :
+class TimeUtils(object):
     def parse_iso8601(text) :
         t=time.strptime(text,  "%Y-%m-%dT%H:%M:%S")
         return datetime(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
     parse_iso8601=staticmethod(parse_iso8601)
 
-import sys
-import os
-import string
-from stat import *
-
 class Volume (File) :
-    def __init__(self,path):
-        if os.path.ismount(path) :        
+    def __init__(self,path, permissive = False):
+        if permissive or os.path.ismount(path) :
             File.__init__(self,path)
         else:
             raise ValueError("path is not a mount point")
-    
+        try:
+            self.getuid = os.getuid
+        except AttributeError:
+            self.getuid = lambda: 0
+
     def sameVolume(self,path) : 
         return Volume.volumeOf(path).path == self.path
 
@@ -362,10 +386,10 @@ class Volume (File) :
 
     def __cmp__(self, other) :
         if not isinstance(other, self.__class__) :
-            return cmp(self, other)
+            return False
         else :
             return cmp(self.path,other.path)
-    
+
     def hasCommonTrashDirectory(self) :
         """
         checks required by trash specification
@@ -380,12 +404,16 @@ class Volume (File) :
         return trashdir.exists() and trashdir.isdir() and not trashdir.islink()
 
     def getCommonTrashDirectory(self) :
-        uid = mygetuid()
-        return TrashDirectory(os.path.join(self.getPath(), ".Trash", str(uid)))
+        uid = self.getuid()
+        trash_directory_path = File(os.path.join(self.getPath(), ".Trash", str(uid)))
+
+        return VolumeTrashDirectory(trash_directory_path,self)
 
     def getUserTrashDirectory(self) :
-        uid = mygetuid()
-        return TrashDirectory(os.path.join(self.getPath(), ".Trash-%s" % str(uid)))
+        uid = self.getuid()
+        trash_directory_path = File(os.path.join(self.getPath(), ".Trash-%s" % str(uid)))
+        
+        return VolumeTrashDirectory(trash_directory_path,self)
 
     # staticmethod
     def volumeOf(path) : 
@@ -414,79 +442,83 @@ class Volume (File) :
     """
     # staticmethod
     def __mount_list():
-      """
-      returns a list of mount points
-      """
-
-
-      doslist=['a:\\','b:\\','c:\\','d:\\','e:\\','f:\\','g:\\','h:\\','i:\\','j:\\','k:\\','l:\\','m:\\','n:\\','o:\\','p:\\','q:\\','r:\\','s:\\','t:\\','u:\\','v:\\','w:\\','x:\\','y:\\','z:\\']
-      mount_list=[]
-
-      """
-      see what kind of system
-      if UNIX like
-         use os.path.ismount(path) from /... use df?
-      if DOS like
-         os.path.exists(path) for  a list of common drive letters
-      """
-      if sys.platform[:3] == 'win':
-        #dos like
-        doslistlen=len(doslist)
-        for apath in doslist:
-          if os.path.exists(apath):
-            #maybe stat check first... yeah, it's there...
-            if os.path.isdir(apath):
-              mode = os.stat(apath)
-              try:
-                 dummy=os.listdir(apath)
-                 mount_list.append(apath)
-              except:
-                 continue
-            else:
-              continue
-        return (mount_list)
-
-      else:
-        #UNIX like
         """
-        AIX and SYSV are somewhat different than the GNU/BSD df, try to catch
-        them. This is for AIX, at this time I don't have a SYS5 available to see
-        what the sys.platform returns... CWR
+        returns a list of mount points
         """
-        if 'aix' in sys.platform.lower():
-          df_file=os.popen('df')
-          while True:
-            df_list=df_file.readline()
-            if not df_list:
-              break #EOF
-            dflistlower = df_list.lower()
-            if 'filesystem' in dflistlower:
-              continue
-            if 'proc' in dflistlower:
-              continue
-
-            file_sys,disc_size,disc_avail,disc_cap_pct,inodes,inodes_pct,mount=df_list.split()
-            mount_list.append(mount)
-
+  
+  
+        doslist=['a:\\','b:\\','c:\\','d:\\','e:\\','f:\\','g:\\','h:\\','i:\\','j:\\','k:\\','l:\\','m:\\','n:\\','o:\\','p:\\','q:\\','r:\\','s:\\','t:\\','u:\\','v:\\','w:\\','x:\\','y:\\','z:\\']
+        mount_list=[]
+  
+        """
+        see what kind of system
+        if UNIX like
+           use os.path.ismount(path) from /... use df?
+        if DOS like
+           os.path.exists(path) for  a list of common drive letters
+        """
+        if sys.platform[:3] == 'win':
+            #dos like
+            doslistlen=len(doslist)
+            for apath in doslist:
+                if os.path.exists(apath):
+                    #maybe stat check first... yeah, it's there...
+                    if os.path.isdir(apath):
+                        mode = os.stat(apath)
+                        try:
+                            dummy=os.listdir(apath)
+                            mount_list.append(apath)
+                        except:
+                            continue
+                    else:
+                        continue
+            return (mount_list)
+  
         else:
-          df_file=os.popen('df -P')
-          while True:
-            df_list=df_file.readline()
-            if not df_list:
-              break #EOF
-            dflistlower = df_list.lower()
-            if 'filesystem' in dflistlower:
-              continue
-            if 'proc' in dflistlower:
-              continue
-
-            file_sys,disc_size,disc_used,disc_avail,disc_cap_pct,mount=df_list.split()
-            mount_list.append(mount)
-
-        return (mount_list)
+            #UNIX like
+            """
+            AIX and SYSV are somewhat different than the GNU/BSD df, try to catch
+            them. This is for AIX, at this time I don't have a SYS5 available to see
+            what the sys.platform returns... CWR
+            """
+            if 'aix' in sys.platform.lower():
+                df_file=os.popen('df')
+                while True:
+                    df_list=df_file.readline()
+                    if not df_list:
+                        break #EOF
+                    dflistlower = df_list.lower()
+                    if 'filesystem' in dflistlower:
+                        continue
+                    if 'proc' in dflistlower:
+                        continue
+    
+                file_sys,disc_size,disc_avail,disc_cap_pct,inodes,inodes_pct,mount=df_list.split()
+                mount_list.append(mount)
+  
+            else:
+                df_file=os.popen('df -P')
+                while True:
+                    df_list=df_file.readline()
+                    if not df_list:
+                        break #EOF
+                    dflistlower = df_list.lower()
+                    if 'filesystem' in dflistlower:
+                        continue
+                    if 'proc' in dflistlower:
+                        continue
+      
+                file_sys,disc_size,disc_used,disc_avail,disc_cap_pct,mount=df_list.split()
+                mount_list.append(mount)
+  
+            return (mount_list)
+    
     __mount_list=staticmethod(__mount_list)
 
     # staticmethod
     def all() :
         return [ Volume(elem) for elem in Volume.__mount_list()]
     all=staticmethod(all)
+
+
+
--- test_libtrash.py
+++ test_libtrash.py
@@ -16,12 +16,6 @@ from exceptions import *
 import os
 import unittest
 
-if dir(os).count('getuid') == 0 :
-    def fake_getuid() :
-        return 999;
-    os.getuid = fake_getuid
-
-
 class TestVolume(unittest.TestCase) :
     def testListVolumes(self) : 
         volumes = Volume.all()
@@ -34,34 +28,49 @@ class TestVolume(unittest.TestCase) :
         v2 = Volume(os.sep)
 
         self.assert_(v1 == v2)
+        
+    def test_getCommonTrashDirectory(self) :
+        instance = Volume("/mnt/disk")
+
+        # invoke
+        result = instance.getCommonTrashDirectory()
+        
+        # check
+        self.assert_(isinstance(result,TrashDirectory))
+        self.assertEqual('/mnt/disk/.Trash/999', result.path.path)
+
+    def test_getCommonTrashDirectory(self) :        
+        instance = Volume("/mnt/disk", True)
+
+        # invoke
+        instance.getuid = lambda : 999
+        result = instance.getUserTrashDirectory()
+        
+        # check
+        self.assert_(isinstance(result,TrashDirectory))
+        self.assertEqual(os.path.abspath('/mnt/disk/.Trash-999'), result.path.path)
+        
 
 class TestTrashDirectory(unittest.TestCase) :
-    def testCreationFromVolume(self) :
-        volume = Volume(os.sep) # main volume ("/"
-
-        # test with /Trash/$uid
-        td = volume.getCommonTrashDirectory()
-        # is a trash direcory
-        self.assert_(isinstance(td,TrashDirectory))
-        # is in the volume
-        self.assert_(td.getPath().startswith(volume.getPath()))
-
-        # test with /Trash-$uid
-        td = volume.getUserTrashDirectory()
-        # is a trash direcory
-        self.assert_(isinstance(td,TrashDirectory))
-        # is in the volume
-        self.assert_(td.getPath().startswith(volume.getPath()))
-
-
+    def test_init(self) :
+        path = File("/mnt/disk/.Trash-123")
+        volume = Volume("/mnt/disk", True);
+        instance = TrashDirectory(path, volume)
+        
+        self.assertEquals(volume,instance.volume)
+        self.assertEquals(path, instance.path)
+        
     def testBasePath(self) :
         os.environ['HOME'] = "/home/test"
         td = TrashDirectory.getHomeTrashDirectory()
         self.assertEqual(td.getBasePath(),os.path.abspath("/"))
         
     def testTrashInfoFileCreation(self) :
-        trashdirectory_base_dir = os.path.realpath("./testTrashDirectory")
-        td = TrashDirectory(trashdirectory_base_dir)
+        trashdirectory_base_dir = File(os.path.realpath("./testTrashDirectory"))
+        volume=File("/")
+        td = TrashDirectory(trashdirectory_base_dir, volume)
+        td._path_for_trashinfo = lambda fileToBeTrashed : "_path_for_trashinfo-result"
+        
         fileToBeTrashed = File("/directory/filetobetrashed.ext")
         deletionTime = datetime(2007, 7, 23, 23, 45, 07)
         trashInfo = td.createTrashInfo(fileToBeTrashed, deletionTime)
@@ -81,8 +90,13 @@ class TestTrashDirectory(unittest.TestCase) :
         trashInfo_as_readed = TrashInfo()
         trashInfo_as_readed.parse(open(trashInfoFilePath).read())
         self.assertEqual(deletionTime, trashInfo_as_readed.getDeletionTime())
-        self.assertEqual(fileToBeTrashed.getPath(),
+        self.assertEqual("_path_for_trashinfo-result", 
                          trashInfo_as_readed.getPath())
+
+    def testCreateTrashInfo(self) :
+        trashdirectory_base_dir = os.path.realpath("./testTrashDirectory")
+        td = TrashDirectory(trashdirectory_base_dir)
+        
 
     def testTrashingFile(self) :
         # create a empty file
@@ -90,11 +104,66 @@ class TestTrashDirectory(unittest.TestCase) :
         open(filename, "w").close()
 
         # trash the file
-        trashDirectory = TrashDirectory("testTrashDirectory")
+        trashDirectory = TrashDirectory(File("testTrashDirectory"), File("/"))
+        trashDirectory._path_for_trashinfo = lambda fileToBeTrashed : "_path_for_trashinfo-result"
         trashDirectory.trash(File(filename))
 
+    def testCreateTrashInfo(self) : 
+        instance = HomeTrashDirectory(File("/home/user/.local/share/Trash"))
+        fileToBeTrashed=File("/home/user/test.txt")
+        deletionTime=datetime(2000,1,1)
+        
+        result=instance.createTrashInfo(fileToBeTrashed, deletionTime)
+        self.assertEquals(datetime(2000,1,1), result.deletionTime)
+        
+        # let test pass also on my windows developing machine
+        if(sys.platform!="win32") :
+            self.assertEquals("/home/user/test.txt",result.getPath())
+        else:
+            self.assertEquals("C:\\home\\user\\test.txt",result.getPath())
+            
+        
+class TestHomeTrashDirectory(unittest.TestCase) :
+    def test_path_for_trashinfo (self) : 
+        instance = HomeTrashDirectory(File("/home/user/.local/share/Trash"))
+        instance.volume = Volume("/")
+
+        # path for HomeTrashDirectory are always absolute
+        fileToBeTrashed=File("/home/user/test.txt")
+        result=instance._path_for_trashinfo(fileToBeTrashed)
+        self.assertEquals(os.path.abspath("/home/user/test.txt"),result)
+            
+        #  ... even if the file is under /home/user/.local/share
+        fileToBeTrashed=File("/home/user/.local/share/test.txt")
+        result=instance._path_for_trashinfo(fileToBeTrashed)
+        self.assertEquals(os.path.abspath("/home/user/.local/share/test.txt"),result)
+
+        
+class TestVolumeTrashDirectory(unittest.TestCase) :
+    def test_init(self) :
+        path = File("/mnt/disk/.Trash/123")
+        volume = Volume("/mnt/disk", True)
+        instance = VolumeTrashDirectory(path, volume)
+        self.assertEquals(path, instance.path)
+        self.assertEquals(volume, instance.volume)
+        
+    def test_path_for_trashinfo (self) : 
+        path = File("/mnt/disk/.Trash-123")
+        volume = Volume("/mnt/volume", True)
+        instance = VolumeTrashDirectory(path, volume)
+
+        # path for VolumeTrashDirectory are relative as possible
+        fileToBeTrashed=File("/mnt/volume/directory/test.txt")
+        result=instance._path_for_trashinfo(fileToBeTrashed)
+        self.assertEquals("directory/test.txt",result)
+            
+        # path for VolumeTrashDirectory are relative as possible
+        fileToBeTrashed=File("/mnt/other-volume/directory/test.txt")
+        result=instance._path_for_trashinfo(fileToBeTrashed)
+        self.assertEquals("/mnt/other-volume/directory/test.txt",result)
+                    
 class TestTrashInfo(unittest.TestCase) :
-    def testCreation(self) :
+    def testParse(self) :
         ti = TrashInfo()
         data = """[Trash Info]
 Path=home%2Fandrea%2Fprova.txt
@@ -116,10 +185,10 @@ class TestTrashedFile(unittest.TestCase) :
         ti.path = "pippo"
         ti.deletionTime = datetime(2007, 7, 23, 23, 45, 07)
         td = TrashDirectory.getHomeTrashDirectory()
-        tf = TrashedFile(ti, td)
+        instance = TrashedFile(ti, td)
         root = os.path.abspath(os.sep)
-        self.assertEqual(tf.getPath(), os.path.join(root,"pippo"))
-        self.assertEqual(ti.getDeletionTime(), tf.getDeletionTime())
+        self.assertEqual(instance.getPath(), os.path.join(root,"pippo"))
+        self.assertEqual(ti.getDeletionTime(), instance.getDeletionTime())
         
 class TestFile(unittest.TestCase) :
     def test_creation(self) :
@@ -141,4 +210,3 @@ class TestTimeUtils(unittest.TestCase) :
 
 if __name__ == "__main__":
     unittest.main()
-    
