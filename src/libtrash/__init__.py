a10c0cc85b1e56c20bc70011e77a9e5261a1739d
andreafrancia <andreafrancia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2008-11-11 21:34:10 +0000

    Removed method samefs because is not used.

--- src/libtrash/__init__.py
+++ src/libtrash/__init__.py
@@ -83,22 +83,6 @@ class Path (object) :
         else :
             raise TypeError("Expected argument type as 'File' or 'str'")
 
-    def samefs(path1, path2):
-        if not (os.path.exists(path1) and os.path.exists(path2)):
-            return False
-
-        while path1 != os.path.dirname(path1):
-            if os.path.ismount(path1):
-                break
-            path1 = os.path.dirname(path1)
-
-        while path2 != os.path.dirname(path2):
-            if os.path.ismount(path2):
-                break
-            path2 = os.path.dirname(path2)
-
-        return path1 == path2  
-
     """
     return Volume the volume where the file is
     """
