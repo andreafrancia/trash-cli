9a32aa14046fd3867cbb895d42b161e1541f942d
andreafrancia <andreafrancia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2008-11-11 21:36:43 +0000

    Restructured the way of starting tests.

--- command-test/mount-test-volume.sh
+++ command-test/mount-test-volume.sh
@@ -20,11 +20,20 @@
 
 set -o errexit
 
-rm -fv test-volume.img
+device_image="$(dirname "$0")/test-volume.img"
+mount_point="$(dirname "$0")/test-volume"
 
-dd if=/dev/zero of=test-volume.img bs=$((1024*1024)) count=1 
-/sbin/mke2fs -F test-volume.img
-mkdir --parents test-volume
-sudo mount -t ext2 test-volume.img test-volume/ -o loop
-sudo chmod a+rwx test-volume
-echo test-volume mounted
+# clean up
+sudo umount "$mount_point"
+rm -fv "$device_image"
+
+# prepare a fresh device image
+dd if=/dev/zero of="$device_image" bs=$((1024*1024)) count=1 
+/sbin/mke2fs -F "$device_image"
+
+# mount
+mkdir --parents "$mount_point"
+sudo mount -t ext2 "$device_image" "$mount_point" -o loop
+sudo chmod a+rwx "$mount_point"
+
+echo "test-volume mounted as '$mount_point'"
