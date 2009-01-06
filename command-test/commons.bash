b6572dcd9375ba783fd15701bcfcff0d12c0c232
andrea.francia <andrea.francia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2009-01-06 20:24:54 +0000

    Fixed commands invocation, now the an invocation notification is echoed to standard error instead of the file with name '2'.

--- command-test/commons.bash
+++ command-test/commons.bash
@@ -30,22 +30,22 @@ except AttributeError:
 
 # --- commands --------------------------------------------
 _trash-put() {
-        echo "Invoking: trash $@" >2
+        echo "Invoking: trash $@" 1>&2
         ../scripts/trash-put "$@"
 }
 
 _trash-empty() {
-        echo "Invoking: trash-empty $@" >2
+        echo "Invoking: trash-empty $@" 1>&2
         ../scripts/trash-empty "$@"
 }
 
 _trash-list() {
-        echo "Invoking: trash-list $@" >2
+        echo "Invoking: trash-list $@" 1>&2
         ../scripts/trash-list "$@"
 }
 
 _trash-restore() {
-        echo "Invoking: trash-restore $@" >2
+        echo "Invoking: trash-restore $@" 1>&2
         ../scripts/trash-restore "$@"
 }
 
