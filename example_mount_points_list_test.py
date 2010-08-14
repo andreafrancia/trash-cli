c6fc1c736b11f0a7749cf2bc08380040855c9757
andreafrancia <andreafrancia@5f8113d0-aa50-0410-8ab7-a55b15e2ca8a>
2010-08-14 21:10:00 +0000

    Working to an portable implementation of list mount points feature

---
+++ example_mount_points_list_test.py
@@ -0,0 +1,66 @@
+import unittest
+import StringIO
+
+class ExampleTest(unittest.TestCase):
+
+    def test_howtoread_stdout_from_process(self):
+	import subprocess
+	pipe = subprocess.Popen(["df", "-P"], stdout=subprocess.PIPE).stdout
+	self.assertEquals('Filesystem         1024-blocks      Used Available Capacity Mounted on\n', pipe.readline());
+	# for line in pipe: 
+	#    print line
+    def test_something(self):
+	'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
+	'/dev/disk0s2         243862672 121934848 121671824      51% /\n'
+	'devfs                      111       111         0     100% /dev\n'
+	'/dev/disk1s1         156287996 123044260  33243736      79% /Volumes/PaccoBello\n'
+
+    def test_should_skip_the_first_line(self):
+	mount_points = mount_points_from_df(StringIO.StringIO(
+	'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
+	))
+
+	self.assertEquals([], list(mount_points))
+
+    def test_should_return_the_first_mount_point(self):
+	mount_points = mount_points_from_df(StringIO.StringIO(
+	'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
+	'/dev/disk0s2         243862672 121934848 121671824      51% /\n'
+	))
+
+	self.assertEquals(['/'], list(mount_points))
+
+    def test_should_return_multiple_mount_point(self):
+	mount_points = mount_points_from_df(StringIO.StringIO(
+	'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
+	'/dev/disk0s2         243862672 121934848 121671824      51% /\n'
+	'/dev/disk1s1         156287996 123044260  33243736      79% /Volumes/DISK\n'
+	))
+
+	self.assertEquals(['/', '/Volumes/DISK'], list(mount_points))
+
+    def test_should_return_mount_point_with_white_spaces(self):
+	mount_points = mount_points_from_df(StringIO.StringIO(
+	'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
+	'/dev/disk0s2         243862672 121934848 121671824      51% /\n'
+	'/dev/disk1s1         156287996 123044260  33243736      79% /Volumes/with white spaces\n'
+	))
+
+	self.assertEquals(['/', '/Volumes/with white spaces'], list(mount_points))
+
+	
+def mount_points_from_df(df_output):
+    def skip_header():
+	df_output.readline()
+    def chomp(string):
+	return string.rstrip('\n')
+	
+    result = []
+    skip_header()
+    for line in df_output:
+	line = chomp(line)	
+	yield line.split(None, 5)[-1] 
+
+
+if __name__ == '__main__':
+    unittest.main()
