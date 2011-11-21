c84302a62e2ed4c015857eccb4cadfd1ae34f118
Andrea Francia <andrea@andreafrancia.it>
2011-11-21 20:27:52 +0100

    Broken apart unit tests from integration tests

---
+++ integration_tests/test_persist.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+from trashcli.trash import TrashDirectory
+from trashcli.trash import TrashInfo
+from trashcli.filesystem import Path
+from trashcli.filesystem import Volume
+
+from datetime import datetime
+import os
+
+class TestTrashDirectory_persit_trash_info(TestCase) :
+    def setUp(self):
+        self.trashdirectory_base_dir = Path(os.path.realpath("./sandbox/testTrashDirectory"))
+        self.trashdirectory_base_dir.remove()
+        
+        self.instance=TrashDirectory(self.trashdirectory_base_dir, Volume(Path("/")))
+        
+    def test_persist_trash_info_first_time(self):
+        trash_info=TrashInfo(Path("dummy-path"), datetime(2007,01,01))
+
+        basename=trash_info.path.basename
+        content=trash_info.render()
+        (trash_info_file,
+                trash_info_id)=self.instance.persist_trash_info(basename,content)
+
+        self.assertTrue(isinstance(trash_info_file, Path))
+        self.assertEquals('dummy-path', trash_info_id)
+        self.assertEquals(self.trashdirectory_base_dir.join('info').join('dummy-path.trashinfo').path, trash_info_file)
+
+        self.assertEquals("""[Trash Info]
+Path=dummy-path
+DeletionDate=2007-01-01T00:00:00
+""", trash_info_file.read())
+
+    def test_persist_trash_info_first_100_times(self):
+        self.test_persist_trash_info_first_time()
+        
+        for i in range(1,100) :
+            trash_info=TrashInfo(Path("dummy-path"), datetime(2007,01,01))
+            
+            basename=trash_info.path.basename
+            content=trash_info.render()
+            (trash_info_file,
+                    trash_info_id)=self.instance.persist_trash_info(basename,content)
+    
+            self.assertEquals('dummy-path'+"_" + str(i), trash_info_id)
+            self.assertEquals("""[Trash Info]
+Path=dummy-path
+DeletionDate=2007-01-01T00:00:00
+""", trash_info_file.read())
+
+    def test_persist_trash_info_other_times(self):
+        self.test_persist_trash_info_first_100_times()
+        
+        for i in range(101,200) :
+            trash_info=TrashInfo(Path("dummy-path"), datetime(2007,01,01))
+            
+            basename=trash_info.path.basename
+            content=trash_info.render()
+            (trash_info_file,
+                    trash_info_id)=self.instance.persist_trash_info(basename,content)
+    
+            self.assertTrue(trash_info_id.startswith("dummy-path_"))
+            self.assertEquals("""[Trash Info]
+Path=dummy-path
+DeletionDate=2007-01-01T00:00:00
+""", trash_info_file.read())
+
+
--- unit_tests/test_trash.py
+++ unit_tests/test_trash.py
@@ -28,15 +28,12 @@ __author__="Andrea Francia (andrea.francia@users.sourceforge.net)"
 __copyright__="Copyright (c) 2007 Andrea Francia"
 __license__="GPL"
 
-import sys
-
 from trashcli.trash import TrashDirectory
 from trashcli.trash import TrashedFile
 from trashcli.trash import TrashInfo
 from trashcli.trash import VolumeTrashDirectory
 from trashcli.trash import TimeUtils
 from trashcli.trash import HomeTrashDirectory
-from trashcli.trash import GlobalTrashCan
 from trashcli.filesystem import Path
 from trashcli.filesystem import Volume
 from trashcli.trash import TopDirIsSymLink
@@ -44,12 +41,9 @@ from trashcli.trash import TopDirNotPresent
 from trashcli.trash import TopDirWithoutStickyBit
 from trashcli.trash import Method1VolumeTrashDirectory
 
-from datetime import *
-from exceptions import *
+from datetime import datetime
 import os
 from unittest import TestCase
-import pdb
-import sys
 from nose.tools import raises
 from nose.tools import assert_equals
 
@@ -109,66 +103,6 @@ class TestTrashDirectory(TestCase) :
         
         assert_equals(Path("/mnt/disk/foo"),
                       instance._calc_original_location(Path("foo")))
-        
-class TestTrashDirectory_persit_trash_info(TestCase) :
-    def setUp(self):
-        self.trashdirectory_base_dir = Path(os.path.realpath("./sandbox/testTrashDirectory"))
-        self.trashdirectory_base_dir.remove()
-        
-        self.instance=TrashDirectory(self.trashdirectory_base_dir, Volume(Path("/")))
-        
-    def test_persist_trash_info_first_time(self):
-        trash_info=TrashInfo(Path("dummy-path"), datetime(2007,01,01))
-
-        basename=trash_info.path.basename
-        content=trash_info.render()
-        (trash_info_file,
-                trash_info_id)=self.instance.persist_trash_info(basename,content)
-
-        self.assertTrue(isinstance(trash_info_file, Path))
-        self.assertEquals('dummy-path', trash_info_id)
-        self.assertEquals(self.trashdirectory_base_dir.join('info').join('dummy-path.trashinfo').path, trash_info_file)
-
-        self.assertEquals("""[Trash Info]
-Path=dummy-path
-DeletionDate=2007-01-01T00:00:00
-""", trash_info_file.read())
-        
-    def test_persist_trash_info_first_100_times(self):
-        self.test_persist_trash_info_first_time()
-        
-        for i in range(1,100) :
-            trash_info=TrashInfo(Path("dummy-path"), datetime(2007,01,01))
-            
-            basename=trash_info.path.basename
-            content=trash_info.render()
-            (trash_info_file,
-                    trash_info_id)=self.instance.persist_trash_info(basename,content)
-    
-            self.assertEquals('dummy-path'+"_" + str(i), trash_info_id)
-            self.assertEquals("""[Trash Info]
-Path=dummy-path
-DeletionDate=2007-01-01T00:00:00
-""", trash_info_file.read())
-
-    def test_persist_trash_info_other_times(self):
-        self.test_persist_trash_info_first_100_times()
-        
-        for i in range(101,200) :
-            trash_info=TrashInfo(Path("dummy-path"), datetime(2007,01,01))
-            
-            basename=trash_info.path.basename
-            content=trash_info.render()
-            (trash_info_file,
-                    trash_info_id)=self.instance.persist_trash_info(basename,content)
-    
-            self.assertTrue(trash_info_id.startswith("dummy-path_"))
-            self.assertEquals("""[Trash Info]
-Path=dummy-path
-DeletionDate=2007-01-01T00:00:00
-""", trash_info_file.read())
-
-
        
 class TestHomeTrashDirectory(TestCase) :
     def test_path_for_trashinfo (self) : 
