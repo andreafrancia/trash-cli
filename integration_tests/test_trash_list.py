026088da37366d39702bf03f30efa08ca57e2262
Andrea Francia <me@andreafrancia.it>
2011-12-14 01:22:33 +0100

    Refactor: test revised

--- integration_tests/test_trash_list.py
+++ integration_tests/test_trash_list.py
@@ -2,54 +2,13 @@ from trashcli.trash2 import ListCmd
 from StringIO import StringIO
 from files import write_file, require_empty_dir
 from nose import SkipTest
+from nose.tools import istest
 
-class TestListCmd_should_list_files:
-    def test_when_there_are_no_files(self):
 
-        self.run()
-
-        self.out.assert_equal_to('')
-
-    def test_when_there_is_one_file(self):
-
-        self.add_trashinfo('/aboslute/path', '2001-02-03T23:55:59')
-
-        self.run()
-
-        self.out.assert_equal_to( "2001-02-03 23:55:59 /aboslute/path\n")
-
-    def test_when_there_are_multiple_files(self):
-        self.add_trashinfo("/file1", "2000-01-01T00:00:01")
-        self.add_trashinfo("/file2", "2000-01-01T00:00:02")
-        self.add_trashinfo("/file3", "2000-01-01T00:00:03")
-
-        self.run()
-
-        self.out.assert_equal_to( "2000-01-01 00:00:01 /file1\n"
-                                  "2000-01-01 00:00:02 /file2\n"
-                                  "2000-01-01 00:00:03 /file3\n")
-
-    def test_it_should_warn_on_badly_formatted_trashinfo(self):
-        self.info_dir.touch('empty.trashinfo')
-
-        self.run()
-
-        self.out.assert_equal_to('')
-        self.err.assert_equal_to(
-                "Parse Error: XDG_DATA_HOME/Trash/info/empty.trashinfo: "
-                "Unable to parse Path\n")
-
-    def test_it_should_warn_on_unreadable_trashinfo(self):
-        self.info_dir.make_unreadable_file('unreadable.trashinfo')
-
-        self.run()
-
-        self.out.assert_equal_to('')
-        self.err.assert_equal_to(
-                "[Errno 13] Permission denied: "
-                "'XDG_DATA_HOME/Trash/info/unreadable.trashinfo'\n")
-
-    def test_it_shows_help_message(self):
+@istest
+class Describe_trash_list_output:
+    @istest
+    def should_output_the_help_message(self):
         self.run('trash-list', '--help')
         self.out.assert_equal_to("""\
 Usage: trash-list [OPTIONS...]
@@ -62,44 +21,82 @@ Options:
 
 Report bugs to http://code.google.com/p/trash-cli/issues
 """)
-        self.err.assert_equal_to('')
 
-    def test_it_shows_files_even_also_deletion_date_is_unparsable(self):
+    @istest
+    def should_output_nothing_if_no_files(self):
+
+        self.run()
+
+        self.out.assert_equal_to('')
+
+    @istest
+    def should_output_deletion_date_and_path_of_trash(self):
+
+        self.add_trashinfo('/aboslute/path', '2001-02-03T23:55:59')
+
+        self.run()
+
+        self.out.assert_equal_to( "2001-02-03 23:55:59 /aboslute/path\n")
+
+    @istest
+    def should_works_also_with_multiple_files(self):
+        self.add_trashinfo("/file1", "2000-01-01T00:00:01")
+        self.add_trashinfo("/file2", "2000-01-01T00:00:02")
+        self.add_trashinfo("/file3", "2000-01-01T00:00:03")
+
+        self.run()
+
+        self.out.assert_equal_to( "2000-01-01 00:00:01 /file1\n"
+                                  "2000-01-01 00:00:02 /file2\n"
+                                  "2000-01-01 00:00:03 /file3\n")
+
+    @istest
+    def should_output_question_mark_if_deletion_date_is_not_present(self):
         self.info_dir.make_file('without-date.trashinfo', 
                 ("[TrashInfo]\n"
                  "Path=/path\n"))
         self.run()
-        self.err.assert_equal_to('')
         self.out.assert_equal_to("????-??-?? ??:??:?? /path\n")
+
+    @istest
+    def should_output_question_marks_if_deletion_date_is_invalid(self):
+        self.info_dir.make_file('without-date.trashinfo', 
+                ("[TrashInfo]\n"
+                 "Path=/path\n"
+                 "DeletionDate=Wrong Date"))
+        self.run()
+        self.out.assert_equal_to("????-??-?? ??:??:?? /path\n")
+
+    @istest
+    def should_warn_about_empty_trashinfos(self):
+        self.info_dir.touch('empty.trashinfo')
+
+        self.run()
+
+        self.err.assert_equal_to(
+                "Parse Error: XDG_DATA_HOME/Trash/info/empty.trashinfo: "
+                "Unable to parse Path\n")
+
+    @istest
+    def should_warn_about_unreadable_trashinfo(self):
+        self.info_dir.make_unreadable_file('unreadable.trashinfo')
+
+        self.run()
+
+        self.err.assert_equal_to(
+                "[Errno 13] Permission denied: "
+                "'XDG_DATA_HOME/Trash/info/unreadable.trashinfo'\n")
 
     def setUp(self):
         self.XDG_DATA_HOME = 'XDG_DATA_HOME'
+        require_empty_dir( self.XDG_DATA_HOME)
 
-        require_empty_dir(self.XDG_DATA_HOME)
-        self.fake_info_dir = FakeInfoDir(self.XDG_DATA_HOME+'/Trash/info')
-        self.add_trashinfo = self.fake_info_dir.add_trashinfo
+        self.info_dir      = FakeInfoDir(self.XDG_DATA_HOME+'/Trash/info')
+        self.add_trashinfo = self.info_dir.add_trashinfo
 
         self.out = OutputCollector()
         self.err = OutputCollector()
 
-        class InfoDir:
-            def __init__(self, XDG_DATA_HOME):
-                self.XDG_DATA_HOME = XDG_DATA_HOME
-            def make_file(self, path_relative_to_info_dir, contents):
-                path = self.real_path(path_relative_to_info_dir)
-                write_file(path, contents)
-            def touch(self, path_relative_to_info_dir):
-                self.make_file(path_relative_to_info_dir, '')
-            def make_unreadable_file(self, path_relative_to_info_dir):
-                path = self.real_path(path_relative_to_info_dir)
-                make_unreadable_file(path)
-            def real_path(self, path_relative_to_info_dir):
-                import os
-                path = os.path.join(self.XDG_DATA_HOME, 'Trash', 'info',
-                                    path_relative_to_info_dir)
-                return path
-
-        self.info_dir = InfoDir(self.XDG_DATA_HOME)
 
     def run(self, *argv):
         ListCmd(
@@ -107,6 +104,69 @@ Report bugs to http://code.google.com/p/trash-cli/issues
             err = self.err,
             environ = {'XDG_DATA_HOME': self.XDG_DATA_HOME}
         ).run(*argv)
+
+@istest
+class Describe_list_trash_on_volume_trashcans:
+    @istest
+    def should_list_method_1_trashcan_contents(self):
+        self.method1.add_trashinfo('file1', '2000-01-01T00:00:00')
+
+        self.run()
+
+        self.out.assert_equal_to("2000-01-01 00:00:00 .fake_root/file1\n")
+
+    @istest
+    def should_list_method2_trashcan_contents(self):
+        self.method2.add_trashinfo('file', '2000-01-01T00:00:00')
+
+        self.run()
+
+        self.out.assert_equal_to("2000-01-01 00:00:00 .fake_root/file\n")
+
+    def setUp(self):
+        self.fake_values = {'uid':123,
+                            'topdir': '.fake_root'}
+
+        require_empty_dir('.fake_root')
+
+        self.method1 = FakeInfoDir( '%(topdir)s/.Trash/%(uid)s/info' %
+                                    self.fake_values)
+
+        self.method2 = FakeInfoDir( '%(topdir)s/.Trash-%(uid)s/info' %
+                                    self.fake_values)
+
+    def run(self, *argv):
+        self.out = OutputCollector()
+        ListCmd(
+            out = self.out,
+            err = StringIO(),
+            environ = {},
+            getuid       = lambda: self.fake_values.get('uid'),
+            list_volumes = lambda: [self.fake_values.get('topdir')],
+        ).run(*argv)
+
+class FakeInfoDir:
+    def __init__(self, path):
+        self.path = path
+        self.number = 1
+    def make_file(self, path_relative_to_info_dir, contents):
+        path = self.real_path(path_relative_to_info_dir)
+        write_file(path, contents)
+    def touch(self, path_relative_to_info_dir):
+        self.make_file(path_relative_to_info_dir, '')
+    def make_unreadable_file(self, path_relative_to_info_dir):
+        path = self.real_path(path_relative_to_info_dir)
+        make_unreadable_file(path)
+    def real_path(self, path_relative_to_info_dir):
+        import os
+        return os.path.join(self.path, path_relative_to_info_dir)
+    def add_file(self, contents):
+        write_file('%(info_dir)s/%(name)s.trashinfo' % {
+            'info_dir' : self.path,
+            'name' : str(self.number)}, contents)
+        self.number = self.number + 1
+    def add_trashinfo(self, escaped_path_entry, formatted_deletion_date):
+        self.add_file(trashinfo(escaped_path_entry, formatted_deletion_date))
 
 def make_unreadable_file(path):
     write_file(path, '')
@@ -120,52 +180,6 @@ def trashinfo(escaped_path_entry, formatted_deletion_date):
     return ("[TrashInfo]\n" + 
             "Path=%s\n" % escaped_path_entry + 
             "DeletionDate=%s\n" % formatted_deletion_date)
-
-class TestListCmd_with_volume_trashcans:
-    def setUp(self):
-        self.fake_uid = 123
-        self.fake_volume = '.fake_root'
-
-        require_empty_dir(self.fake_volume)
-
-        self.fake_info_dir1 = FakeInfoDir(
-                '%(volume)s/.Trash/%(uid)s/info' % {
-                    'volume' : self.fake_volume,
-                    'uid'    : self.fake_uid})
-
-        self.fake_info_dir2 = FakeInfoDir(
-                '%(volume)s/.Trash-%(uid)s/info' % {
-                    'volume' : self.fake_volume,
-                    'uid'    : self.fake_uid})
-
-    def test_it_lists_trash_from_method1_trashcan(self):
-        self.fake_info_dir1.add_trashinfo('file1', '2000-01-01T00:00:00')
-
-        self.out = OutputCollector()
-        ListCmd(
-            out          = self.out,
-            err          = StringIO(),
-            environ      = {},
-            getuid       = lambda: 123,
-            list_volumes = lambda: ['.fake_root'],
-        ).run()
-
-        self.out.assert_equal_to("2000-01-01 00:00:00 .fake_root/file1\n")
-
-    def test_it_lists_trash_from_method2_trashcan(self):
-        self.fake_info_dir2.add_trashinfo('file', '2000-01-01T00:00:00')
-
-        self.out = OutputCollector()
-        ListCmd(
-            out          = self.out,
-            err          = StringIO(),
-            environ      = {},
-            getuid       = lambda: 123,
-            list_volumes = lambda: ['.fake_root'],
-        ).run()
-
-        self.out.assert_equal_to("2000-01-01 00:00:00 .fake_root/file\n")
-
 
 class OutputCollector:
     def __init__(self):
@@ -181,17 +195,4 @@ class OutputCollector:
         text = self.stream.getvalue()
         from nose.tools import assert_regexp_matches
         assert_regexp_matches(text, regex)
-
-class FakeInfoDir:
-    def __init__(self, path):
-        self.path   = path
-        self.number = 1
-
-    def add_file(self, contents):
-        write_file('%(info_dir)s/%(name)s.trashinfo' % {
-            'info_dir' : self.path,
-            'name' : str(self.number)}, contents)
-        self.number = self.number + 1
-    def add_trashinfo(self, escaped_path_entry, formatted_deletion_date):
-        self.add_file(trashinfo(escaped_path_entry, formatted_deletion_date))
 
