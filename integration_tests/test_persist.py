9dab55774d726d01de5b61affaa5afcab2085383
Andrea Francia <andrea@andreafrancia.it>
2016-12-17 20:06:06 +0100

    Refactor

--- integration_tests/test_persist.py
+++ integration_tests/test_persist.py
@@ -5,7 +5,7 @@ import os
 from nose.tools import assert_equals, assert_true
 
 from integration_tests.files import require_empty_dir
-from trashcli.put import TrashDirectoryForPut
+from trashcli.put import TrashDirectoryForPut, RealFs
 
 join = os.path.join
 
@@ -18,7 +18,8 @@ class TestTrashDirectory_persit_trash_info:
         self.instance = TrashDirectoryForPut(
                 self.trashdirectory_base_dir,
                 "/",
-                None)
+                None,
+                RealFs())
 
     def persist_trash_info(self, basename, content):
         return self.instance.persist_trash_info(
