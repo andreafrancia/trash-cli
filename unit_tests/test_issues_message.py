5f41b7805fbdad0ff20e1231a81a1d097ea187ff
Andrea Francia <andrea@andreafrancia.it>
2016-12-19 22:48:53 +0100

    Add tests for the last line of the output of all commands that currently report where to put issues

---
+++ unit_tests/test_issues_message.py
@@ -0,0 +1,45 @@
+from nose.tools import assert_equals
+
+from StringIO import StringIO
+
+class TestTrashPutIssueMessage:
+
+    def setUp(self):
+        self.out = StringIO()
+
+    def test_trash_put_last_line(self):
+        from trashcli.put import TrashPutCmd
+
+        cmd = TrashPutCmd(self.out, StringIO(), None, None)
+        cmd.run(['', '--help'])
+
+        self.assert_last_line_of_output_is(
+                'Report bugs to http://code.google.com/p/trash-cli/issues')
+
+    def test_trash_empty_last_line(self):
+        from trashcli.trash import EmptyCmd
+
+        cmd = EmptyCmd(self.out, StringIO(), [], lambda:[])
+        cmd.run('', '--help')
+
+        self.assert_last_line_of_output_is(
+                'Report bugs to http://code.google.com/p/trash-cli/issues')
+
+    def test_trash_list_last_line(self):
+        from trashcli.trash import ListCmd
+
+        cmd = ListCmd(self.out, None, None, None, None)
+        cmd.run('', '--help')
+
+        self.assert_last_line_of_output_is(
+                'Report bugs to http://code.google.com/p/trash-cli/issues')
+
+    def assert_last_line_of_output_is(self, expected):
+        output = self.out.getvalue()
+        if len(output.splitlines()) > 0:
+            last_line = output.splitlines()[-1]
+        else:
+            last_line = ''
+        assert_equals(expected, last_line,
+                ('Last line of output should be:\n\n%s\n\n' % expected +
+                'but the output is\n\n%s' % output))
