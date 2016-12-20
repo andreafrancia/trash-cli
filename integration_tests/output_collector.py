05c609ae24c899d3df9900e3c6b94fa56febced9
Andrea Francia <andrea@andreafrancia.it>
2016-12-20 13:46:15 +0100

    Refactor

--- integration_tests/output_collector.py
+++ integration_tests/output_collector.py
@@ -10,9 +10,11 @@ class OutputCollector:
     def assert_equal_to(self, expected):
         return self.should_be(expected)
     def should_be(self, expected):
-        assert_equals_with_unidiff(expected, self.stream.getvalue())
+        assert_equals_with_unidiff(expected, self.output())
     def should_match(self, regex):
-        text = self.stream.getvalue()
+        text = self.output()
         from nose.tools import assert_regexp_matches
         assert_regexp_matches(text, regex)
+    def output(self):
+        return self.stream.getvalue()
 
