1f0e2fc5e5890e00de7df86722aa348cbc24a5e2
Andrea Francia <andrea@andreafrancia.it>
2016-12-22 20:58:34 +0100

    Add test

--- unit_tests/test_restore_cmd.py
+++ unit_tests/test_restore_cmd.py
@@ -3,16 +3,45 @@ from nose.tools import assert_equals
 from StringIO import StringIO
 
 class TestTrashRestoreCmd:
+    def setUp(self):
+        self.stdout = StringIO()
+        self.cmd = RestoreCmd(stdout=self.stdout,
+                              stderr=None,
+                              environ=None,
+                              exit = None,
+                              input=None,
+                              version = None)
+
     def test_should_print_version(self):
-        stdout = StringIO()
-        cmd = RestoreCmd(stdout=stdout,
-                         stderr=None,
-                         environ=None,
-                         exit = None,
-                         input=None,
-                         version = '1.2.3')
+        self.cmd.version = '1.2.3'
+        self.cmd.run(['trash-restore', '--version'])
 
-        cmd.run(['trash-restore', '--version'])
+        assert_equals('trash-restore 1.2.3\n', self.stdout.getvalue())
 
-        assert_equals('trash-restore 1.2.3\n', stdout.getvalue())
+    def test_with_no_args_and_no_files_in_trashcan(self):
+        self.cmd.trashcan.home_trashcan.environ = {}
+        self.cmd.curdir = lambda: "cwd"
 
+        self.cmd.run(['trash-restore'])
+
+        assert_equals("No files trashed from current dir ('cwd')\n",
+                self.stdout.getvalue())
+
+    def test_with_no_args_and_files_in_trashcan(self):
+        class thing(object):
+            pass
+        def some_files(append, _):
+            t = thing()
+            t.deletion_date = None
+            t.path = None
+            append(t)
+        self.cmd.trashcan.home_trashcan.environ = {}
+        self.cmd.curdir = lambda: "cwd"
+        self.cmd.for_all_trashed_file_in_dir = some_files
+        self.cmd.input = lambda _ : ""
+
+        self.cmd.run(['trash-restore'])
+
+        assert_equals("   0 None None\n"
+                "Exiting\n",
+                self.stdout.getvalue())
