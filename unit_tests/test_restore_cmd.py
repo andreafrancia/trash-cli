f84a974e83cab3e39bab6a16e9bb5eda46f1e49f
Andrea Francia <andrea@andreafrancia.it>
2016-12-24 17:18:22 +0100

    Refactor

--- unit_tests/test_restore_cmd.py
+++ unit_tests/test_restore_cmd.py
@@ -186,9 +186,11 @@ class Test_create_trashed_file_from_info_file:
         trash_dir.volume = '/volume'
         trash_dir.all_info_files = Mock([], return_value=[path_to_trashinfo])
         cmd.all_trash_directories = lambda x,y: [trash_dir]
+        trashed_files = []
 
-        trashed_file = list(cmd.all_trashed_files())[0]
+        trashed_files = list(cmd.all_trashed_files())
 
+        trashed_file = trashed_files[0]
         assert_equals('/volume/name' , trashed_file.path)
         assert_equals(datetime.datetime(2001, 1, 1, 10, 10, 10),
                       trashed_file.deletion_date)
