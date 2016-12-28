047e6be650ad6de422e2ec062dd29514ba24647e
Andrea Francia <andrea@andreafrancia.it>
2016-12-28 19:49:42 +0100

    Refactor

--- trashcli/empty.py
+++ trashcli/empty.py
@@ -29,11 +29,9 @@ class EmptyCmd:
                                    list_volumes = list_volumes,
                                    top_trashdir_rules = top_trashdir_rules)
         self.version      = version
-        self.trashcan     = CleanableTrashcan(file_remover)
         self._now         = now
-        self._expiry_date = ExpiryDate(self.file_reader,
-                                       self._now,
-                                       self.trashcan)
+        self._dustman     = DeleteAlways()
+        self.file_remover = file_remover
 
     def run(self, *argv):
         self.exit_code     = EX_OK
@@ -41,13 +39,19 @@ class EmptyCmd:
         parse = Parser()
         parse.on_help(PrintHelp(self.description, self.println))
         parse.on_version(PrintVersion(self.println, self.version))
-        parse.on_argument(self._expiry_date.set_max_age_in_days)
-        parse.as_default(self._empty_all_trashdirs)
+        parse.on_argument(self.set_max_age_in_days)
+        parse.as_default(self.empty_all_trashdirs)
         parse.on_invalid_option(self.report_invalid_option_usage)
 
         parse(argv)
 
         return self.exit_code
+
+    def set_max_age_in_days(self, arg):
+        max_age_in_days = int(arg)
+        self._dustman = DeleteAccordingDate(self.file_reader.contents_of,
+                                                 self._now,
+                                                 max_age_in_days)
 
     def report_invalid_option_usage(self, program_name, option):
         self.err.write(
@@ -61,10 +65,13 @@ class EmptyCmd:
            "  --version   show program's version number and exit",
            "  -h, --help  show this help message and exit")
         printer.bug_reporting()
-    def _empty_all_trashdirs(self):
+    def empty_all_trashdirs(self):
+        trashcan = CleanableTrashcan(self.file_remover)
+        def delete_if_expired(trashinfo_path):
+            self._dustman.delete_if_ok(trashinfo_path, trashcan)
         harvester = Harvester(self.file_reader)
-        harvester.on_trashinfo_found = self._expiry_date.delete_if_expired
-        harvester.on_orphan_found = self.trashcan.delete_orphan
+        harvester.on_trashinfo_found = delete_if_expired
+        harvester.on_orphan_found = trashcan.delete_orphan
         self.trashdirs.on_trash_dir_found = harvester.analize_trash_directory
         self.trashdirs.list_trashdirs()
     def println(self, line):
@@ -86,20 +93,6 @@ class DeleteAccordingDate:
 class DeleteAlways:
     def delete_if_ok(self, trashinfo_path, trashcan):
         trashcan.delete_trashinfo_and_backup_copy(trashinfo_path)
-
-class ExpiryDate:
-    def __init__(self, file_reader, now, trashcan):
-        self.file_reader = file_reader
-        self._now        = now
-        self._dustman    = DeleteAlways()
-        self.trashcan    = trashcan
-    def set_max_age_in_days(self, arg):
-        max_age_in_days = int(arg)
-        self._dustman = DeleteAccordingDate(self.file_reader.contents_of,
-                                                 self._now,
-                                                 max_age_in_days)
-    def delete_if_expired(self, trashinfo_path):
-        self._dustman.delete_if_ok(trashinfo_path, self.trashcan)
 
 class IfDate:
     def __init__(self, date_criteria, then):
