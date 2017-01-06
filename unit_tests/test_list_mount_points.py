6f9d95577b3dc1f0b9f40d9574bd844acc06817f
Andrea Francia <andrea@andreafrancia.it>
2017-01-06 15:49:17 +0100

    Another fix for python 3, introduced MyStringIO

--- unit_tests/test_list_mount_points.py
+++ unit_tests/test_list_mount_points.py
@@ -1,20 +1,24 @@
 # Copyright (C) 2011 Andrea Francia Trivolzio(PV) Italy
 
 import unittest
-from unit_tests.myStringIO import StringIO
+from io import BytesIO
 from trashcli.list_mount_points import _mount_points_from_df_output
+
+class MyStringIO(BytesIO):
+    def __init__(self, string):
+        super(MyStringIO, self).__init__(string.encode('ascii', 'strict'))
 
 class MountPointFromDirTest(unittest.TestCase):
 
     def test_should_skip_the_first_line(self):
-        mount_points = _mount_points_from_df_output(StringIO(
+        mount_points = _mount_points_from_df_output(MyStringIO(
         'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
         ))
 
         self.assertEquals([], list(mount_points))
 
     def test_should_return_the_first_mount_point(self):
-        mount_points = _mount_points_from_df_output(StringIO(
+        mount_points = _mount_points_from_df_output(MyStringIO(
         'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
         '/dev/disk0s2         243862672 121934848 121671824      51% /\n'
         ))
@@ -22,7 +26,7 @@ class MountPointFromDirTest(unittest.TestCase):
         self.assertEquals(['/'], list(mount_points))
 
     def test_should_return_multiple_mount_point(self):
-        mount_points = _mount_points_from_df_output(StringIO(
+        mount_points = _mount_points_from_df_output(MyStringIO(
         'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
         '/dev/disk0s2         243862672 121934848 121671824      51% /\n'
         '/dev/disk1s1         156287996 123044260  33243736      79% /Volumes/DISK\n'
@@ -31,7 +35,7 @@ class MountPointFromDirTest(unittest.TestCase):
         self.assertEquals(['/', '/Volumes/DISK'], list(mount_points))
 
     def test_should_return_mount_point_with_white_spaces(self):
-        mount_points = _mount_points_from_df_output(StringIO(
+        mount_points = _mount_points_from_df_output(MyStringIO(
         'Filesystem         1024-blocks      Used Available Capacity Mounted on\n'
         '/dev/disk0s2         243862672 121934848 121671824      51% /\n'
         '/dev/disk1s1         156287996 123044260  33243736      79% /Volumes/with white spaces\n'
