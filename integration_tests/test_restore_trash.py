43525f6604f3db46d3275403c7f92bee4c7c02e6
Andrea Francia <andrea@andreafrancia.it>
2020-11-08 22:36:47 +0100

    Refactor: review test_restore_trash.py (2)

--- integration_tests/test_restore_trash.py
+++ integration_tests/test_restore_trash.py
@@ -18,7 +18,7 @@ class TestRestoreTrash(unittest.TestCase):
                          self.user.stdout())
 
     def test_gives_an_error_on_not_a_number_input(self):
-        self.having_a_trashed_file('/foo/bar')
+        self.user.having_a_trashed_file('/foo/bar')
         self.user.chdir('/foo')
 
         self.user.run_restore(with_user_typing='-@notanumber')
@@ -26,7 +26,7 @@ class TestRestoreTrash(unittest.TestCase):
         self.assertEqual('Invalid entry\n', self.user.stderr())
 
     def test_it_gives_error_when_user_input_is_too_small(self):
-        self.having_a_trashed_file('/foo/bar')
+        self.user.having_a_trashed_file('/foo/bar')
         self.user.chdir('/foo')
 
         self.user.run_restore(with_user_typing='-1')
@@ -34,7 +34,7 @@ class TestRestoreTrash(unittest.TestCase):
         self.assertEqual('Invalid entry\n', self.user.stderr())
 
     def test_it_gives_error_when_user_input_is_too_large(self):
-        self.having_a_trashed_file('/foo/bar')
+        self.user.having_a_trashed_file('/foo/bar')
         self.user.chdir('/foo')
 
         self.user.run_restore(with_user_typing='1')
@@ -42,7 +42,7 @@ class TestRestoreTrash(unittest.TestCase):
         self.assertEqual('Invalid entry\n', self.user.stderr())
 
     def test_it_shows_the_file_deleted_from_the_current_dir(self):
-        self.having_a_trashed_file('/foo/bar')
+        self.user.having_a_trashed_file('/foo/bar')
         self.user.chdir('/foo')
 
         self.user.run_restore(with_user_typing='')
@@ -52,7 +52,7 @@ class TestRestoreTrash(unittest.TestCase):
         self.assertEqual('', self.user.stderr())
 
     def test_it_restores_the_file_selected_by_the_user(self):
-        self.having_a_file_trashed_from_current_dir('foo')
+        self.user.having_a_file_trashed_from_current_dir('foo')
         self.user.chdir(os.getcwd())
 
         self.user.run_restore(with_user_typing='0')
@@ -60,7 +60,7 @@ class TestRestoreTrash(unittest.TestCase):
         self.file_should_have_been_restored('foo')
 
     def test_it_refuses_overwriting_existing_file(self):
-        self.having_a_file_trashed_from_current_dir('foo')
+        self.user.having_a_file_trashed_from_current_dir('foo')
         self.user.chdir(os.getcwd())
         write_file("foo")
 
@@ -71,16 +71,7 @@ class TestRestoreTrash(unittest.TestCase):
 
     def setUp(self):
         require_empty_dir('XDG_DATA_HOME')
-
-        trashcan = TrashCan('XDG_DATA_HOME/Trash')
-        self.having_a_trashed_file = trashcan.write_trashed_file
-
         self.user = RestoreTrashUser('XDG_DATA_HOME')
-
-    def having_a_file_trashed_from_current_dir(self, filename):
-        self.having_a_trashed_file(os.path.join(os.getcwd(), filename))
-        remove_file(filename)
-        assert not os.path.exists(filename)
 
     def file_should_have_been_restored(self, filename):
         assert os.path.exists(filename)
@@ -88,7 +79,7 @@ class TestRestoreTrash(unittest.TestCase):
 
 class RestoreTrashUser:
     def __init__(self, XDG_DATA_HOME):
-        self.environ = {'XDG_DATA_HOME': XDG_DATA_HOME}
+        self.XDG_DATA_HOME = XDG_DATA_HOME
         self.out = StringIO()
         self.err = StringIO()
 
@@ -99,23 +90,27 @@ class RestoreTrashUser:
         RestoreCmd(
             stdout  = self.out,
             stderr  = self.err,
-            environ = self.environ,
+            environ = {'XDG_DATA_HOME': self.XDG_DATA_HOME},
             exit    = [].append,
             input   = lambda msg: with_user_typing,
             curdir  = lambda: self.current_dir
         ).run([])
+
+    def having_a_file_trashed_from_current_dir(self, filename):
+        self.having_a_trashed_file(os.path.join(os.getcwd(), filename))
+        remove_file(filename)
+        assert not os.path.exists(filename)
+
+    def having_a_trashed_file(self, path):
+        write_file('%s/info/foo.trashinfo' % self._trash_dir(),
+                   a_trashinfo(path))
+        write_file('%s/files/foo' % self._trash_dir())
+
+    def _trash_dir(self):
+        return "%s/Trash" % self.XDG_DATA_HOME
 
     def stdout(self):
         return self.out.getvalue()
 
     def stderr(self):
         return self.err.getvalue()
-
-
-class TrashCan:
-    def __init__(self, path):
-        self.path = path
-
-    def write_trashed_file(self, path):
-        write_file('%s/info/foo.trashinfo' % self.path, a_trashinfo(path))
-        write_file('%s/files/foo' % self.path)
