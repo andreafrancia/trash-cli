df1b10a3b4e124b0f7a746d541a344f3a1538b11
Andrea Francia <andrea@andreafrancia.it>
2020-11-26 21:47:30 +0100

    Refactor: removed unused methods assert_equal_to, should_match

--- integration_tests/output_collector.py
+++ integration_tests/output_collector.py
@@ -7,14 +7,8 @@ class OutputCollector:
         self.getvalue = self.stream.getvalue
     def write(self,data):
         self.stream.write(data)
-    def assert_equal_to(self, expected):
-        return self.should_be(expected)
     def should_be(self, expected):
         assert_equals_with_unidiff(expected, self.output())
-    def should_match(self, regex):
-        text = self.output()
-        from nose.tools import assert_regexp_matches
-        assert_regexp_matches(text, regex)
     def output(self):
         return self.stream.getvalue()
 
