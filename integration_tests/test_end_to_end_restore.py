e5834a9b6cb9a968cd0f248bb8fc3c84b538ab51
Andrea Francia <andrea@andreafrancia.it>
2020-11-26 21:05:36 +0100

    Refactor: compatibility with python 2.7 in tests, using Popen instead of subprocess.run

--- integration_tests/test_end_to_end_restore.py
+++ integration_tests/test_end_to_end_restore.py
@@ -2,8 +2,6 @@ import shutil
 import unittest
 import subprocess
 from subprocess import PIPE
-
-from integration_tests.files import write_file
 from trashcli import base_dir
 import tempfile
 import os
@@ -20,9 +18,16 @@ No files trashed from current dir ('%s')
 """ % self.tmpdir, result.stdout.decode('utf-8'))
 
     def run_command(self, command):
+        class Result:
+            def __init__(self, stdout, stderr):
+                self.stdout = stdout
+                self.stderr = stderr
         command_full_path = os.path.join(base_dir, command)
-        return subprocess.run(["python", command_full_path],
-                              stdout=PIPE, stderr=PIPE, cwd=self.tmpdir)
+        process = subprocess.Popen(["python", command_full_path], stdout=PIPE,
+                                   stderr=PIPE, cwd=self.tmpdir)
+        stdout, stderr = process.communicate()
+
+        return Result(stdout, stderr)
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
