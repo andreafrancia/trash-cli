1ed3a9a81a46f32c5235def2cef5dc1e3a2096c5
Andrea Francia <andrea@andreafrancia.it>
2020-12-10 19:25:41 +0100

    Fix declaration for python 2.7

--- integration_tests/test_end_to_end_restore.py
+++ integration_tests/test_end_to_end_restore.py
@@ -26,7 +26,7 @@ No files trashed from current dir ('%s')
         trash_dir = FakeTrashDir(self.trash_dir)
         trash_dir.add_trashinfo(a_trashinfo("/path"))
 
-        result = self.run_command("trash-restore", "/", input='0')
+        result = self.run_command("trash-restore", ["/"], input='0')
 
         self.assertEqual("""\
    0 2000-01-01 00:00:01 /path
@@ -36,14 +36,16 @@ What file to restore [0..0]: """,
                          self.trash_dir,
                          result.stderr.decode('utf-8'))
 
-    def run_command(self, command, *args, input=''):
+    def run_command(self, command, args=None, input=''):
         class Result:
             def __init__(self, stdout, stderr):
                 self.stdout = stdout
                 self.stderr = stderr
+        if args == None:
+            args = []
         command_full_path = os.path.join(base_dir, command)
         process = subprocess.Popen(["python", command_full_path,
-                                    "--trash-dir", self.trash_dir] + list(args),
+                                    "--trash-dir", self.trash_dir] + args,
                                    stdin=PIPE,
                                    stdout=PIPE,
                                    stderr=PIPE, cwd=self.curdir)
