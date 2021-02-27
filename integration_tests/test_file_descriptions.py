f707edb3233baf2d60a13293e9315d3404954c4f
Andrea Francia <andrea@andreafrancia.it>
2021-02-27 19:01:28 +0100

    Refactor: TestDescriptions

--- integration_tests/test_file_descriptions.py
+++ integration_tests/test_file_descriptions.py
@@ -1,54 +1,50 @@
-# Copyright (C) 2011 Andrea Francia Trivolzio(PV) Italy
+# Copyright (C) 2011-2021 Andrea Francia Bereguardo(PV) Italy
 
 from trashcli.put import describe
-from .files import require_empty_dir, make_empty_file
+from .files import require_empty_dir, make_empty_file, MyPath, make_file
 import os
 import unittest
 
-class TestDescritions(unittest.TestCase):
+
+class TestDescriptions(unittest.TestCase):
     def setUp(self):
-        require_empty_dir('sandbox')
+        self.temp_dir = MyPath.make_temp_dir()
+        require_empty_dir(self.temp_dir / 'sandbox')
 
     def test_on_directories(self):
 
         assert "directory" == describe('.')
         assert "directory" == describe("..")
-        assert "directory" == describe("sandbox")
+        assert "directory" == describe(self.temp_dir / 'sandbox')
 
     def test_on_dot_directories(self):
 
-        assert "'.' directory" ==  describe("sandbox/.")
+        assert "'.' directory" ==  describe(self.temp_dir / "sandbox/.")
         assert "'.' directory" ==  describe("./.")
 
     def test_on_dot_dot_directories(self):
 
         assert "'..' directory" == describe("./..")
-        assert "'..' directory" == describe("sandbox/..")
+        assert "'..' directory" == describe(self.temp_dir / "sandbox/..")
 
     def test_name_for_regular_files_non_empty_files(self):
 
-        write_file("sandbox/non-empty", "contents")
-        assert "regular file" == describe("sandbox/non-empty")
+        make_file(self.temp_dir / "sandbox/non-empty", "contents")
+        assert "regular file" == describe(self.temp_dir / "sandbox/non-empty")
 
     def test_name_for_empty_file(self):
 
-        make_empty_file('sandbox/empty')
-        assert "regular empty file" == describe("sandbox/empty")
+        make_empty_file(self.temp_dir / 'sandbox/empty')
+        assert "regular empty file" == describe(self.temp_dir / "sandbox/empty")
 
     def test_name_for_symbolic_links(self):
 
-        os.symlink('nowhere', "sandbox/symlink")
+        os.symlink('nowhere', self.temp_dir / "sandbox/symlink")
 
-        assert "symbolic link" == describe("sandbox/symlink")
+        assert "symbolic link" == describe(self.temp_dir / "sandbox/symlink")
 
     def test_name_for_non_existent_entries(self):
 
-        assert not os.path.exists('non-existent')
+        assert not os.path.exists(self.temp_dir / 'non-existent')
 
-        assert "non existent" == describe('non-existent')
-
-def write_file(path, contents):
-    f = open(path, 'w')
-    f.write(contents)
-    f.close()
-
+        assert "non existent" == describe(self.temp_dir / 'non-existent')
