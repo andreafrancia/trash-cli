c572f00beee07bcbbe6da48251c584a498e557e7
Andrea Francia <andrea@andreafrancia.it>
2021-02-27 19:25:34 +0100

    Refactor: using temp dir in test_trash_directory.py

--- integration_tests/restore/test_trash_directory.py
+++ integration_tests/restore/test_trash_directory.py
@@ -4,50 +4,50 @@ import six
 
 from trashcli.restore import TrashDirectory
 
-from integration_tests.files import require_empty_dir
+from integration_tests.files import require_empty_dir, MyPath
 from integration_tests.files import make_file
 from mock import Mock
 
 
 class TestTrashDirectory(unittest.TestCase):
     def setUp(self):
-        require_empty_dir('sandbox')
+        self.temp_dir = MyPath.make_temp_dir()
+        require_empty_dir(self.temp_dir / 'trash-dir')
         self.trash_dir = TrashDirectory()
         self.logger = Mock()
         self.trash_dir.logger = self.logger
 
-
     def test_should_list_a_trashinfo(self):
-        make_file('sandbox/info/foo.trashinfo')
+        make_file(self.temp_dir / 'trash-dir/info/foo.trashinfo')
 
         result = self.list_trashinfos()
 
-        assert [('trashinfo', 'sandbox/info/foo.trashinfo')] == result
+        assert [('trashinfo', self.temp_dir / 'trash-dir/info/foo.trashinfo')] == result
 
     def test_should_list_multiple_trashinfo(self):
-        make_file('sandbox/info/foo.trashinfo')
-        make_file('sandbox/info/bar.trashinfo')
-        make_file('sandbox/info/baz.trashinfo')
+        make_file(self.temp_dir / 'trash-dir/info/foo.trashinfo')
+        make_file(self.temp_dir / 'trash-dir/info/bar.trashinfo')
+        make_file(self.temp_dir / 'trash-dir/info/baz.trashinfo')
 
         result = self.list_trashinfos()
 
         six.assertCountEqual(self,
-                             [('trashinfo', 'sandbox/info/foo.trashinfo'),
-                              ('trashinfo', 'sandbox/info/baz.trashinfo'),
-                              ('trashinfo', 'sandbox/info/bar.trashinfo')],
+                             [('trashinfo', self.temp_dir / 'trash-dir/info/foo.trashinfo'),
+                              ('trashinfo', self.temp_dir / 'trash-dir/info/baz.trashinfo'),
+                              ('trashinfo', self.temp_dir / 'trash-dir/info/bar.trashinfo')],
                              result)
 
     def test_non_trashinfo_should_reported_as_a_warn(self):
-        make_file('sandbox/info/not-a-trashinfo')
+        make_file(self.temp_dir / 'trash-dir/info/not-a-trashinfo')
 
         result = self.list_trashinfos()
 
         six.assertCountEqual(self,
                              [('non_trashinfo',
-                               'sandbox/info/not-a-trashinfo')],
+                               self.temp_dir / 'trash-dir/info/not-a-trashinfo')],
                              result)
 
     def list_trashinfos(self):
-        return list(self.trash_dir.all_info_files('sandbox'))
+        return list(self.trash_dir.all_info_files(self.temp_dir / 'trash-dir'))
 
 
