0a81b13db1b8da47540b1404102b4981f8e3a209
Andrea Francia <andrea@andreafrancia.it>
2021-04-07 20:16:00 +0200

    Refactor: add tests for atomic_write

---
+++ integration_tests/put/test_atomic_write.py
@@ -0,0 +1,39 @@
+import errno
+import unittest
+
+from trashcli.fs import atomic_write, read_file
+from unit_tests.support import MyPath
+
+
+class Test_atomic_write(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = MyPath.make_temp_dir()
+
+    def test_short_filename(self):
+        path = self.temp_dir / 'a'
+
+        atomic_write(path, b'contents')
+
+        assert 'contents' == read_file(path)
+
+    def test_too_long_filename(self):
+        path = self.temp_dir / ('a' * 2000)
+
+        try:
+            atomic_write(path, b'contents')
+            self.fail()
+        except OSError as e:
+            assert e.errno == errno.ENAMETOOLONG
+
+    def test_filename_already_taken(self):
+        atomic_write(self.temp_dir / "a", b'contents')
+
+        try:
+            atomic_write(self.temp_dir / "a", b'contents')
+            self.fail()
+        except OSError as e:
+            assert e.errno == errno.EEXIST
+
+
+    def tearDown(self):
+        self.temp_dir.clean_up()
