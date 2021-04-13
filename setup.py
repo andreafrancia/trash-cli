f5c6af105ac134b7dde5c52e5e9e9f36bc6affd7
Andrea Francia <andrea@andreafrancia.it>
2021-04-13 18:56:04 +0200

    Refactor: applying pep8 to setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,49 @@
 # Copyright (C) 2007-2021 Andrea Francia Trivolzio(PV) Italy
 
+import os
+import stat
+from textwrap import dedent
 from distutils.core import setup
 
 from trashcli.fs import read_file, write_file
+from trashcli import trash
 
 
 def main():
-    from trashcli import trash
-    scripts.add_script('trash'        , 'trashcli.put' , 'main')
-    scripts.add_script('trash-put'    , 'trashcli.put' , 'main')
-    scripts.add_script('trash-list'   , 'trashcli.list', 'main')
+    scripts.add_script('trash', 'trashcli.put', 'main')
+    scripts.add_script('trash-put', 'trashcli.put', 'main')
+    scripts.add_script('trash-list', 'trashcli.list', 'main')
     scripts.add_script('trash-restore', 'trashcli.restore', 'main')
-    scripts.add_script('trash-empty'  , 'trashcli.empty', 'main')
-    scripts.add_script('trash-rm'     , 'trashcli.rm'  , 'main')
+    scripts.add_script('trash-empty', 'trashcli.empty', 'main')
+    scripts.add_script('trash-rm', 'trashcli.rm', 'main')
     setup(
-        name = 'trash-cli'        , version = trash.version                  ,
-        author = 'Andrea Francia' , author_email = 'andrea@andreafrancia.it' ,
-        url = 'https://github.com/andreafrancia/trash-cli',
-        description = 'Command line interface to FreeDesktop.org Trash.',
-        long_description = read_file("README.rst"),
-        license = 'GPL v2',
-        packages = ['trashcli'],
-        scripts = scripts.created_scripts,
-        data_files = [('share/man/man1', ['man/man1/trash-empty.1',
-                                          'man/man1/trash-list.1',
-                                          'man/man1/trash-restore.1',
-                                          'man/man1/trash-put.1',
-                                          'man/man1/trash-rm.1'])],
+        name='trash-cli',
+        version=trash.version,
+        author='Andrea Francia',
+        author_email='andrea@andreafrancia.it',
+        url='https://github.com/andreafrancia/trash-cli',
+        description='Command line interface to FreeDesktop.org Trash.',
+        long_description=read_file("README.rst"),
+        license='GPL v2',
+        packages=['trashcli'],
+        scripts=scripts.created_scripts,
+        data_files=[('share/man/man1', ['man/man1/trash-empty.1',
+                                        'man/man1/trash-list.1',
+                                        'man/man1/trash-restore.1',
+                                        'man/man1/trash-put.1',
+                                        'man/man1/trash-rm.1'])],
         install_requires=[
             'psutil',
         ],
     )
 
-from textwrap import dedent
+
 class Scripts:
     def __init__(self, write_file, make_file_executable):
-        self.write_file           = write_file
+        self.write_file = write_file
         self.make_file_executable = make_file_executable
-        self.created_scripts      = []
+        self.created_scripts = []
+
     def add_script(self, name, module, main_function):
         script_contents = dedent("""\
             #!/usr/bin/env python
@@ -50,7 +56,7 @@ class Scripts:
         self.make_file_executable(name)
         self.created_scripts.append(name)
 
-import os,stat
+
 def make_file_executable(path):
     os.chmod(path, os.stat(path).st_mode | stat.S_IXUSR)
 
