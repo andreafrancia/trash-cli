d197c5b968fa31f19c3f8ab16da95d22cf2f1311
Andrea Francia <andrea@andreafrancia.it>
2021-05-28 18:48:44 +0200

    Refactor: add tests to TestEmptyEndToEnd

--- tests/empty/test_empty_end_to_end.py
+++ tests/empty/test_empty_end_to_end.py
@@ -1,4 +1,6 @@
 import unittest
+
+from trashcli import trash
 from .. import run_command
 from ..support import MyPath
 
@@ -7,7 +9,7 @@ class TestEmptyEndToEnd(unittest.TestCase):
     def setUp(self):
         self.tmp_dir = MyPath.make_temp_dir()
 
-    def test(self):
+    def test_help(self):
         result = run_command.run_command(self.tmp_dir, "trash-empty",
                                          ['--help'])
         self.assertEqual(["""\
@@ -21,6 +23,32 @@ Options:
 
 Report bugs to https://github.com/andreafrancia/trash-cli/issues
 """, '', 0],
+                         [result.stdout,
+                          result.stderr,
+                          result.exit_code])
+
+    def test_h(self):
+        result = run_command.run_command(self.tmp_dir, "trash-empty",
+                                         ['-h'])
+        self.assertEqual(["""\
+Usage: trash-empty [days]
+
+Purge trashed files.
+
+Options:
+  --version   show program's version number and exit
+  -h, --help  show this help message and exit
+
+Report bugs to https://github.com/andreafrancia/trash-cli/issues
+""", '', 0],
+                         [result.stdout,
+                          result.stderr,
+                          result.exit_code])
+
+    def test_version(self):
+        result = run_command.run_command(self.tmp_dir, "trash-empty",
+                                         ['--version'])
+        self.assertEqual(['trash-empty %s\n' % trash.version, '', 0],
                          [result.stdout,
                           result.stderr,
                           result.exit_code])
