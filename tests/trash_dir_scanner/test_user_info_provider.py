a84b63d11f4ffa68cc57d6837fda804dc518e9ad
Andrea Francia <andrea@andreafrancia.it>
2021-06-28 19:23:05 +0200

    Refactor: add test: TestUserInfoProvider

---
+++ tests/trash_dir_scanner/test_user_info_provider.py
@@ -0,0 +1,20 @@
+import unittest
+
+from trashcli.trash import UserInfoProvider
+
+
+class TestUserInfoProvider(unittest.TestCase):
+    def test_getuid(self):
+        provider = UserInfoProvider({}, lambda: 123)
+
+        info = provider.get_user_info()
+
+        assert [123] == [i.getuid() for i in info]
+
+    def test_home(self):
+        provider = UserInfoProvider({'HOME':"~"}, lambda: 123)
+
+        info = provider.get_user_info()
+
+        assert [['~/.local/share/Trash']] == \
+               [i.home_trash_dir_paths() for i in info]
