49a0337d4c3dfc49946152bc97c1f4caae70936b
Andrea Francia <andrea@andreafrancia.it>
2021-08-11 15:07:36 +0200

    Refactor: first draft of TestEmptyEndToEndInteractive

---
+++ tests/empty/test_empty_end_to_end_interactive.py
@@ -0,0 +1,34 @@
+import datetime
+import unittest
+
+import pytest
+
+from ..fake_trash_dir import FakeTrashDir
+from ..support import MyPath, list_trash_dir
+from .. import run_command
+
+
+@pytest.mark.slow
+class TestEmptyEndToEndInteractive(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = MyPath.make_temp_dir()
+        self.xdg_data_home = self.tmp_dir / 'XDG_DATA_HOME'
+        self.environ = {'XDG_DATA_HOME': self.xdg_data_home}
+        self.trash_dir = self.xdg_data_home / 'Trash'
+        self.fake_trash_dir = FakeTrashDir(self.trash_dir)
+
+    def user_run_trash_empty(self, args):
+        return run_command.run_command(self.tmp_dir, "trash-empty",
+                                       args, env=self.environ)
+
+    def set_clock_at(self, yyyy_mm_dd):
+        self.environ['TRASH_DATE'] = '%sT00:00:00' % yyyy_mm_dd
+
+    def test_it_should_keep_files_newer_than_N_days(self):
+        self.fake_trash_dir.add_trashinfo_with_date('foo', datetime.date(2000, 1, 1))
+        self.set_clock_at('2000-01-01')
+
+        result = self.user_run_trash_empty(['-i'])
+
+        assert ['trash-empty: error: unrecognized arguments: -i',
+                2] == [result.stderr.splitlines()[-1], result.exit_code]
