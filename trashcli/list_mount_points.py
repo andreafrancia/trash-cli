63a53b9919e75ce4a4d2d372fda30b3eb563412e
Andrea Francia <andrea@andreafrancia.it>
2022-04-16 23:13:28 +0200

    In docker don't list volume files (like /etc/resolv.conf)

--- trashcli/list_mount_points.py
+++ trashcli/list_mount_points.py
@@ -1,4 +1,7 @@
 # Copyright (C) 2009-2020 Andrea Francia Trivolzio(PV) Italy
+import os.path
+
+
 def main():
     for mp in os_mount_points():
         print(mp)
@@ -7,7 +10,8 @@ def main():
 def os_mount_points():
     import psutil
     for p in psutil.disk_partitions():
-        yield p.mountpoint
+        if os.path.isdir(p.mountpoint):
+            yield p.mountpoint
 
 
 if __name__ == "__main__":
