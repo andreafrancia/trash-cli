11553a084d4789ae6ea2c1bc5c414a3236109feb
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-04 14:48:50 +0200

    Remove (broken) support for building rpm packages (with python setup.py bdist)

--- install-rpm.sh
+++
@@ -1,3 +0,0 @@
-python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
-# 'brp-compress' gzips the man pages without distutils knowing... fix this
-sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
