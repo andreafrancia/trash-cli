41a1656d4b047098101d251c42bfe5df970821fa
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-04 15:03:29 +0200

    Revert "Remove setup.py add pyproject.toml"
    This reverts commit 1adef7f2be609bf761a24c306db981625fe706a8.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2007-2021 Andrea Francia Trivolzio(PV) Italy
+
+from trashcli.fs import read_file
+
+
+def main():
+    setup()
+
+
+if __name__ == '__main__':
+    from setuptools import setup
+
+    main()
