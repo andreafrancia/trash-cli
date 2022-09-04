0cd89e3677bb929ca6eab42f3d45418f64e83c7b
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-04 14:19:39 +0200

    Refactor: move other values to setup.cfg (from setup.py)

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@ from trashcli.fs import read_file
 
 def main():
     setup(
-        author='Andrea Francia',
-        author_email='andrea@andreafrancia.it',
-        url='https://github.com/andreafrancia/trash-cli',
-        description='Command line interface to FreeDesktop.org Trash.',
-        long_description=read_file("README.rst"),
-        license='GPL v2',
         packages=['trashcli'],
         data_files=[('share/man/man1', ['man/man1/trash-empty.1',
                                         'man/man1/trash-list.1',
