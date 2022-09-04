c37fd03b8f02ad7e1b6d541dac8dc12357285050
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-04 14:34:24 +0200

    Refactor: move data_files option to setup.cfg

--- setup.py
+++ setup.py
@@ -4,14 +4,7 @@ from trashcli.fs import read_file
 
 
 def main():
-    setup(
-        data_files=[('share/man/man1', ['man/man1/trash-empty.1',
-                                        'man/man1/trash-list.1',
-                                        'man/man1/trash-restore.1',
-                                        'man/man1/trash.1',
-                                        'man/man1/trash-put.1',
-                                        'man/man1/trash-rm.1'])],
-    )
+    setup()
 
 
 if __name__ == '__main__':
