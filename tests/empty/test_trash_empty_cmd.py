5dc13360adaffdcb0e146b6fb8368200e8d0c681
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-14 21:43:01 +0200

    Refactor: one TestCase class for each test file.

---
+++ tests/empty/test_trash_empty_cmd.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2011-2022 Andrea Francia Bereguardo(PV) Italy
+import unittest
+
+import pytest
+from mock import Mock
+from six import StringIO
+
+from tests.files import make_unreadable_dir, make_readable
+from tests.support import MyPath, volumes_mock
+from trashcli.empty.empty_cmd import EmptyCmd
+from trashcli.fs import FileRemover, FileSystemContentReader, \
+    FileSystemDirReader, TopTrashDirRulesFileSystemReader
+from trashcli.fstab import VolumesListing
+
+
+@pytest.mark.slow
+class TestTrashEmptyCmd(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = MyPath.make_temp_dir()
+        self.unreadable_dir = self.tmp_dir / 'data/Trash/files/unreadable'
+        self.volumes_listing = Mock(spec=VolumesListing)
+        self.volumes_listing.list_volumes.return_value = [self.unreadable_dir]
+        self.err = StringIO()
+        self.environ = {'XDG_DATA_HOME': self.tmp_dir / 'data'}
+        self.empty = EmptyCmd(
+            argv0='trash-empty',
+            out=StringIO(),
+            err=self.err,
+            volumes_listing=self.volumes_listing,
+            now=None,
+            file_reader=TopTrashDirRulesFileSystemReader(),
+            file_remover=FileRemover(),
+            content_reader=FileSystemContentReader(),
+            dir_reader=FileSystemDirReader(),
+            version=None,
+            volumes=volumes_mock()
+        )
+
+    def test_trash_empty_will_skip_unreadable_dir(self):
+        make_unreadable_dir(self.unreadable_dir)
+
+        self.empty.run_cmd([], self.environ, uid=123)
+
+        assert ("trash-empty: cannot remove %s\n" % self.unreadable_dir ==
+                self.err.getvalue())
+
+    def tearDown(self):
+        make_readable(self.unreadable_dir)
+        self.tmp_dir.clean_up()
--- tests/empty/test_trash_empty_cmd_with_multiple_volumes.py
+++ tests/empty/test_trash_empty_cmd_with_multiple_volumes.py
@@ -2,53 +2,16 @@
 import os
 import unittest
 
-import pytest
 from mock import Mock
 from six import StringIO
 
+from tests.files import require_empty_dir, make_dirs, set_sticky_bit, \
+    make_empty_file
+from tests.support import MyPath, volumes_mock
 from trashcli.empty.empty_cmd import EmptyCmd
 from trashcli.fs import FileRemover, FileSystemContentReader, \
     FileSystemDirReader, TopTrashDirRulesFileSystemReader
 from trashcli.fstab import VolumesListing
-from .files import require_empty_dir, make_dirs, set_sticky_bit, \
-    make_unreadable_dir, make_empty_file, make_readable
-from .support import MyPath, volumes_mock
-
-
-@pytest.mark.slow
-class TestTrashEmptyCmd(unittest.TestCase):
-    def setUp(self):
-        self.tmp_dir = MyPath.make_temp_dir()
-        self.unreadable_dir = self.tmp_dir / 'data/Trash/files/unreadable'
-        self.volumes_listing = Mock(spec=VolumesListing)
-        self.volumes_listing.list_volumes.return_value = [self.unreadable_dir]
-        self.err = StringIO()
-        self.environ = {'XDG_DATA_HOME': self.tmp_dir / 'data'}
-        self.empty = EmptyCmd(
-            argv0='trash-empty',
-            out=StringIO(),
-            err=self.err,
-            volumes_listing=self.volumes_listing,
-            now=None,
-            file_reader=TopTrashDirRulesFileSystemReader(),
-            file_remover=FileRemover(),
-            content_reader=FileSystemContentReader(),
-            dir_reader=FileSystemDirReader(),
-            version=None,
-            volumes=volumes_mock()
-        )
-
-    def test_trash_empty_will_skip_unreadable_dir(self):
-        make_unreadable_dir(self.unreadable_dir)
-
-        self.empty.run_cmd([], self.environ, uid=123)
-
-        assert ("trash-empty: cannot remove %s\n" % self.unreadable_dir ==
-                self.err.getvalue())
-
-    def tearDown(self):
-        make_readable(self.unreadable_dir)
-        self.tmp_dir.clean_up()
 
 
 class TestEmptyCmdWithMultipleVolumes(unittest.TestCase):
