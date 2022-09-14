9bb62ffef39e8af50975e97283de50c2a119b123
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-14 21:56:42 +0200

    Refactor: in emptier.py

--- trashcli/empty/emptier.py
+++ trashcli/empty/emptier.py
@@ -9,13 +9,12 @@ class Emptier:
 
     def do_empty(self, trash_dirs, environ, parsed_days):
         for trash_dir in only_found(trash_dirs):  # type: TrashDir
-            trash_dir_path = trash_dir.path
-            for trashinfo_path in self.trash_dir_reader.list_trashinfo(
-                    trash_dir_path):
-                if self.delete_mode.ok_to_delete(trashinfo_path, environ,
+            for trash_info_path in self.trash_dir_reader.list_trashinfo(
+                    trash_dir.path):
+                if self.delete_mode.ok_to_delete(trash_info_path, environ,
                                                  parsed_days):
                     self.trashcan.delete_trashinfo_and_backup_copy(
-                        trashinfo_path)
+                        trash_info_path)
             for orphan in self.trash_dir_reader.list_orphans(
-                    trash_dir_path):
+                    trash_dir.path):
                 self.trashcan.delete_orphan(orphan)
