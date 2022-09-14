4b7eae49e8f7cf61b4cd2c3f943062752258c409
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-14 22:08:39 +0200

    Refactor: add test for FileRemover.remove_file_if_exists

--- tests/test_file_remover.py
+++ tests/test_file_remover.py
@@ -7,3 +7,7 @@ class TestFileRemover(unittest.TestCase):
     def test_remove_file_fails_when_file_does_not_exists(self):
         file_remover = FileRemover()
         self.assertRaises(FileNotFoundError, file_remover.remove_file, '/non/existing/path')
+
+    def test_remove_file_if_exists_fails_when_file_does_not_exists(self):
+        file_remover = FileRemover()
+        file_remover.remove_file_if_exists('/non/existing/path')
