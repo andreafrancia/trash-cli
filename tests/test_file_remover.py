887bc7707bf48219b9148f935b0fba9f02729171
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-15 22:16:18 +0200

    Compatibility for python 2.7 in test_file_remover.py (FileNotFoundError)

--- tests/test_file_remover.py
+++ tests/test_file_remover.py
@@ -2,11 +2,17 @@ import unittest
 
 from trashcli.fs import FileRemover
 
+try:
+    FileNotFoundError  # python 2
+except NameError:
+    FileNotFoundError = IOError
+
 
 class TestFileRemover(unittest.TestCase):
     def test_remove_file_fails_when_file_does_not_exists(self):
         file_remover = FileRemover()
-        self.assertRaises(FileNotFoundError, file_remover.remove_file, '/non/existing/path')
+        self.assertRaises(FileNotFoundError, file_remover.remove_file,
+                          '/non/existing/path')
 
     def test_remove_file_if_exists_fails_when_file_does_not_exists(self):
         file_remover = FileRemover()
