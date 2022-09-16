4b2ef28d0bde2b9aa07c7e46c52404f181b85320
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-09-16 23:35:33 +0200

    Refactor: test_empty_end_to_end_with_trash_dir.py

--- tests/empty/test_empty_end_to_end.py
+++ tests/empty/test_empty_end_to_end.py
@@ -1,12 +1,9 @@
-import os
 import unittest
 
 from trashcli import trash
 from .. import run_command
 from ..run_command import normalize_options
-from ..fake_trash_dir import FakeTrashDir
-from ..files import make_file
-from ..support import MyPath, list_trash_dir
+from ..support import MyPath
 
 
 class TestEmptyEndToEnd(unittest.TestCase):
@@ -88,61 +85,6 @@ Report bugs to https://github.com/andreafrancia/trash-cli/issues
         self.assertEqual(['trash-empty: invalid TRASH_DATE: not a valid date\n',
                           0],
                          [result.stderr, result.exit_code])
-
-    def tearDown(self):
-        self.tmp_dir.clean_up()
-
-
-class TestEmptyEndToEndWithTrashDir(unittest.TestCase):
-    def setUp(self):
-        self.tmp_dir = MyPath.make_temp_dir()
-        self.trash_dir = self.tmp_dir / 'trash-dir'
-        self.fake_trash_dir = FakeTrashDir(self.trash_dir)
-
-    def test_add_trashed_file(self):
-        self.fake_trash_dir.add_trashed_file('foo', '/foo', 'FOO')
-
-        assert list_trash_dir(self.trash_dir) == ['info/foo.trashinfo',
-                                                  'files/foo']
-
-    def test_trash_dir(self):
-        self.fake_trash_dir.add_trashed_file('foo', '/foo', 'FOO')
-
-        result = run_command.run_command(self.tmp_dir, "trash-empty",
-                                         ['--trash-dir', self.trash_dir])
-
-        assert [result.all, list_trash_dir(self.trash_dir)] == \
-               [['', '', 0], []]
-
-    def test_xdg_data_home(self):
-        xdg_data_home = self.tmp_dir / 'xdg'
-        FakeTrashDir(xdg_data_home / 'Trash').add_trashed_file('foo', '/foo', 'FOO')
-
-        result = run_command.run_command(self.tmp_dir, "trash-empty",
-                                         [], env={'XDG_DATA_HOME': xdg_data_home})
-
-        trash_dir = xdg_data_home / 'Trash'
-        assert [result.all, list_trash_dir(trash_dir)] == \
-               [['', '', 0], []]
-
-    def test_non_trash_info_is_not_deleted(self):
-        make_file(self.trash_dir / 'info' / 'non-trashinfo')
-
-        result = run_command.run_command(self.tmp_dir, "trash-empty",
-                                         ['--trash-dir', self.trash_dir])
-
-        assert [result.all, list_trash_dir(self.trash_dir)] == \
-               [['', '', 0], ['info/non-trashinfo']]
-
-    def test_orphan_are_deleted(self):
-        make_file(self.trash_dir / 'files' / 'orphan')
-        os.makedirs(self.trash_dir / 'files' / 'orphan dir')
-
-        result = run_command.run_command(self.tmp_dir, "trash-empty",
-                                         ['--trash-dir', self.trash_dir])
-
-        assert [result.all, list_trash_dir(self.trash_dir)] == \
-               [['', '', 0], []]
 
     def tearDown(self):
         self.tmp_dir.clean_up()
---
+++ tests/empty/test_empty_end_to_end_with_trash_dir.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+
+from tests import run_command
+from tests.fake_trash_dir import FakeTrashDir
+from tests.files import make_file
+from tests.support import MyPath, list_trash_dir
+
+
+class TestEmptyEndToEndWithTrashDir(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = MyPath.make_temp_dir()
+        self.trash_dir = self.tmp_dir / 'trash-dir'
+        self.fake_trash_dir = FakeTrashDir(self.trash_dir)
+
+    def test_add_trashed_file(self):
+        self.fake_trash_dir.add_trashed_file('foo', '/foo', 'FOO')
+
+        assert list_trash_dir(self.trash_dir) == ['info/foo.trashinfo',
+                                                  'files/foo']
+
+    def test_trash_dir(self):
+        self.fake_trash_dir.add_trashed_file('foo', '/foo', 'FOO')
+
+        result = run_command.run_command(self.tmp_dir, "trash-empty",
+                                         ['--trash-dir', self.trash_dir])
+
+        assert [result.all, list_trash_dir(self.trash_dir)] == \
+               [['', '', 0], []]
+
+    def test_xdg_data_home(self):
+        xdg_data_home = self.tmp_dir / 'xdg'
+        FakeTrashDir(xdg_data_home / 'Trash').add_trashed_file('foo', '/foo',
+                                                               'FOO')
+
+        result = run_command.run_command(self.tmp_dir, "trash-empty",
+                                         [],
+                                         env={'XDG_DATA_HOME': xdg_data_home})
+
+        trash_dir = xdg_data_home / 'Trash'
+        assert [result.all, list_trash_dir(trash_dir)] == \
+               [['', '', 0], []]
+
+    def test_non_trash_info_is_not_deleted(self):
+        make_file(self.trash_dir / 'info' / 'non-trashinfo')
+
+        result = run_command.run_command(self.tmp_dir, "trash-empty",
+                                         ['--trash-dir', self.trash_dir])
+
+        assert [result.all, list_trash_dir(self.trash_dir)] == \
+               [['', '', 0], ['info/non-trashinfo']]
+
+    def test_orphan_are_deleted(self):
+        make_file(self.trash_dir / 'files' / 'orphan')
+        os.makedirs(self.trash_dir / 'files' / 'orphan dir')
+
+        result = run_command.run_command(self.tmp_dir, "trash-empty",
+                                         ['--trash-dir', self.trash_dir])
+
+        assert [result.all, list_trash_dir(self.trash_dir)] == \
+               [['', '', 0], []]
+
+    def tearDown(self):
+        self.tmp_dir.clean_up()
