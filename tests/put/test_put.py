1366af3c8d48f073d58844c137ffb9b0a14ed5df
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-10-12 19:01:04 +0200

    Refactor: test_put.py checks also exit_code

--- tests/put/test_put.py
+++ tests/put/test_put.py
@@ -9,6 +9,7 @@ from tests.put.support.fake_fs.fake_fs import FakeFs
 from trashcli.fstab import create_fake_volume_of
 from trashcli.put.access import Access
 from trashcli.put.main import do_main
+from trashcli.trash import EX_IOERR
 
 
 class TestPut(unittest.TestCase):
@@ -21,22 +22,24 @@ class TestPut(unittest.TestCase):
         volumes = create_fake_volume_of(['/'])
         stderr = StringIO()
         err = None
+        exit_code = None
         try:
-            result = do_main(access=access,
-                             argv=['trash-put', '-vvv', 'file'],
-                             clock=clock,
-                             environ={},
-                             fs=fs,
-                             my_input=my_input,
-                             randint=randint,
-                             stderr=stderr,
-                             uid=123,
-                             volumes=volumes)
+            exit_code = do_main(access=access,
+                                argv=['trash-put', '-vvv', 'file'],
+                                clock=clock,
+                                environ={},
+                                fs=fs,
+                                my_input=my_input,
+                                randint=randint,
+                                stderr=stderr,
+                                uid=123,
+                                volumes=volumes)
         except IOError as e:
             err = e
         assert [
                    stderr.getvalue().splitlines(),
                    str(err),
+                   exit_code,
                ] == [
                    ['trash-put: volume of file: /',
                     'trash-put: found unusable .Trash dir (should be a dir): /.Trash',
@@ -46,4 +49,5 @@ class TestPut(unittest.TestCase):
                     'trash-put: failed to trash file in /.Trash-123, because: no such file or directory: file',
                     "trash-put: cannot trash non existent 'file'"],
                    'None',
+                   EX_IOERR
                ]
