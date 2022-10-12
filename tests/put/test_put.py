c048b846238bfc7d9365644035d974870c9015d2
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-10-12 22:30:25 +0200

    Refactor: characterization test "test_put_home" for test_put.py

--- tests/put/test_put.py
+++ tests/put/test_put.py
@@ -41,6 +41,28 @@ class TestPut(unittest.TestCase):
             EX_IOERR
         ]
 
+    def test_put_home(self):
+        result = self.run_cmd(['trash-put', '-vvv', 'file'],
+                              {"HOME": "/home/user"}, 123)
+
+        assert result == [
+            ['trash-put: volume of file: /',
+             'trash-put: trying trash dir: /home/user/.local/share/Trash from volume: /',
+             'trash-put: .trashinfo created as '
+             '/home/user/.local/share/Trash/info/file.trashinfo.',
+             'trash-put: failed to trash file in ~/.local/share/Trash, because: no such '
+             'file or directory: file',
+             'trash-put: found unusable .Trash dir (should be a dir): /.Trash',
+             'trash-put: trash directory /.Trash/123 is not secure',
+             'trash-put: trying trash dir: /.Trash-123 from volume: /',
+             'trash-put: .trashinfo created as /.Trash-123/info/file.trashinfo.',
+             'trash-put: failed to trash file in /.Trash-123, because: no such file or '
+             'directory: file',
+             "trash-put: cannot trash non existent 'file'"],
+            'None',
+            EX_IOERR
+        ]
+
     def run_cmd(self, args, environ, uid):
         err = None
         exit_code = None
