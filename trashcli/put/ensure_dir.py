2b268672bebbaeab5ee254c0e9b9b8ee3d097be2
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-10-20 13:05:59 +0200

    Refactor: add type hinting Fs to EnsureDir initializer

--- trashcli/put/ensure_dir.py
+++ trashcli/put/ensure_dir.py
@@ -1,5 +1,8 @@
+from trashcli.put.fs import Fs
+
+
 class EnsureDir:
-    def __init__(self, fs):
+    def __init__(self, fs): # type: (Fs) -> None
         self.fs = fs
 
     def ensure_dir(self, path, mode):
