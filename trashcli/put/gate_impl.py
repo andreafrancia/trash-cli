f321ac58821e419842d570691db02109860ae114
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-11-11 21:12:16 +0100

    Refactor: add __repr__ method to GateCheckResult

--- trashcli/put/gate_impl.py
+++ trashcli/put/gate_impl.py
@@ -19,6 +19,12 @@ class GateCheckResult:
     def error(reason):
         return GateCheckResult(False, reason)
 
+    def __repr__(self):
+        if self.ok and self.reason is None:
+            return 'GateCheckResult.ok()'
+        if not self.ok and self.reason is not None:
+            return 'GateCheckResult.error(%r)' % self.reason
+        return 'GateCheckResult(%s, %r)' % (self.ok, self.reason)
 
 class GateImpl(object):
     @staticmethod
