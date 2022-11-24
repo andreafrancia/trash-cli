ac8326a0ac5a714f5fbaa04f278d0c0e185e2d31
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2022-11-24 12:08:34 +0100

    Characterization of the bug reported in #268

--- tests/test_put/test_trash_put_script.py
+++ tests/test_put/test_trash_put_script.py
@@ -1,12 +1,17 @@
+import os
 import unittest
 
 import pytest
 
 from tests.run_command import run_command
+from tests.support.files import make_file
+from tests.support.my_path import MyPath
 
 
 @pytest.mark.slow
 class TestPutScripts(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = MyPath.make_temp_dir()
 
     def test_trash_put_works(self):
         result = run_command('.', 'trash-put')
@@ -15,5 +20,29 @@ class TestPutScripts(unittest.TestCase):
 
     def test_trash_put_touch_filesystem(self):
         result = run_command('.', 'trash-put', ['non-existent'])
-        assert ("trash-put: cannot trash non existent 'non-existent'\n" ==
-                result.stderr)
+        assert (result.stderr ==
+                "trash-put: cannot trash non existent 'non-existent'\n")
+
+    def test_trashing_danglig_symlink_bug(self):
+        os.symlink('non-existent', self.tmp_dir / 'dangling-link')
+
+        result = run_command(self.tmp_dir, 'trash-put', ['dangling-link'])
+
+        assert (result.stderr ==
+                "trash-put: cannot trash symbolic link 'dangling-link'\n")
+        assert os.path.lexists(self.tmp_dir / 'dangling-link')
+
+    def test_trashing_normal_symlink(self):
+        make_file(self.tmp_dir / 'link-target')
+        os.symlink('link-target', self.tmp_dir / 'normal-link')
+
+        result = run_command(self.tmp_dir, 'trash-put',
+                             ['-v',
+                              'normal-link',
+                              '--trash-dir', self.tmp_dir / 'trash-dir',
+                              ])
+
+        assert ("trash-put: 'normal-link' trashed in %s" % (self.tmp_dir / 'trash-dir')
+                in result.stderr.splitlines())
+        assert (result.stdout.splitlines() == [])
+        assert not os.path.lexists(self.tmp_dir / 'normal-link')
