709c7afc49c4eb086c9ed6cac53171fa30a9c50c
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2023-05-12 21:42:41 +0200

    Refactor: Sequences implemented with NamedTuple

--- trashcli/restore/sequences.py
+++ trashcli/restore/sequences.py
@@ -1,18 +1,10 @@
-class Sequences:
-    def __init__(self, sequences):
-        self.sequences = sequences
+from typing import NamedTuple
 
-    def __repr__(self):
-        return "Sequences(%s)" % repr(self.sequences)
 
+class Sequences(NamedTuple('Sequences', [
+    ('sequences', list),
+])):
     def all_indexes(self):
         for sequence in self.sequences:
             for index in sequence:
                 yield index
-
-    def __eq__(self, other):
-        if type(other) != type(self):
-            return False
-        if self.sequences != other.sequences:
-            return False
-        return True
