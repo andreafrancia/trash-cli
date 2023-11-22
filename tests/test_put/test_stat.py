b9bb01c4c9c45c4b4f896023909de6b02f3269c9
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2023-11-22 23:54:49 +0100

    Refactor: octal function

--- tests/test_put/test_stat.py
+++ tests/test_put/test_stat.py
@@ -2,6 +2,25 @@ from tests.run_command import temp_dir
 from trashcli.put.fs.real_fs import RealFs
 
 
+def octal(n):  # type: (int) -> str
+    o = oct(n)
+
+    def remove_octal_prefix(o):
+        if o.startswith('0o'):
+            return o[2:]
+        elif o.startswith('0'):
+            return o[1:]
+        else:
+            ValueError('Invalid octal format: %s' % o)
+
+    return "%s%s" % ("0o", remove_octal_prefix(o))
+
+
+class TestOctal:
+    def test(self):
+        assert octal(16877) == '0o40755'
+
+
 class TestStat:
     def test(self, temp_dir):
         fs = RealFs()
