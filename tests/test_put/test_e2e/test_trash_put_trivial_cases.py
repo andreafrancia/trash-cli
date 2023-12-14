d55d824c39a3f885c41764df1925c680a6a2b346
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2023-12-14 17:28:28 +0100

    Removed duplicated test

--- tests/test_put/test_e2e/test_trash_put_trivial_cases.py
+++ tests/test_put/test_e2e/test_trash_put_trivial_cases.py
@@ -9,12 +9,7 @@ fs = RealFs()
 
 @pytest.mark.slow
 class TestTrashPutTrivial:
-    def test_trash_put_works(self):
-        result = run_command('.', 'trash-put')
-        assert ("usage: trash-put [OPTION]... FILE..." in
-                result.stderr.splitlines())
-
-    def test_trash_put_touch_filesystem(self):
+    def test_trash_put_touch_filesystem(self, temp_dir):
         result = run_command('.', 'trash-put', ['non-existent'])
         assert (result.stderr ==
                 "trash-put: cannot trash non existent 'non-existent'\n")
