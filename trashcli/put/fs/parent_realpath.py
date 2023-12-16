f058bad52102d47e3dd2cb6ff16728132feabc26
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2023-12-16 17:59:12 +0100

    Refactor: add type hint

--- trashcli/put/fs/parent_realpath.py
+++ trashcli/put/fs/parent_realpath.py
@@ -1,8 +1,10 @@
 import os
 
+from trashcli.put.fs.fs import RealPathFs
+
 
 class ParentRealpathFs:
-    def __init__(self, fs):
+    def __init__(self, fs):  # type: (RealPathFs) -> None
         self.fs = fs
 
     def parent_realpath(self, path):
