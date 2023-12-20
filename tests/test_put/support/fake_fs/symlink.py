061d0f2cb58c8efa046edb5ea9b67d06a63a2760
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2023-12-20 21:32:17 +0100

    Refactor: add __repr__ for SymLink

--- tests/test_put/support/fake_fs/symlink.py
+++ tests/test_put/support/fake_fs/symlink.py
@@ -4,3 +4,6 @@ from tests.test_put.support.fake_fs.ent import Ent
 class SymLink(Ent):
     def __init__(self, dest):
         self.dest = dest
+
+    def __repr__(self):
+        return "SymLink(%r)" % self.dest
