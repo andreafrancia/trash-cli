361a509f05374633fad73244c1fe73ea7c774ddd
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2023-12-20 21:31:10 +0100

    Refactor: formatting file

--- tests/output_collector.py
+++ tests/output_collector.py
@@ -7,9 +7,12 @@ class OutputCollector:
     def __init__(self):
         self.stream = StringIO()
         self.getvalue = self.stream.getvalue
-    def write(self,data):
+
+    def write(self, data):
         self.stream.write(data)
+
     def should_be(self, expected):
         assert_equals_with_unidiff(expected, self.output())
+
     def output(self):
         return self.stream.getvalue()
