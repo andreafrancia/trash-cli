b7c37a9866a13f62a60e898478cbf60683bcad46
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2024-03-25 21:33:16 +0100

    Revise test and introduce DirectoryLayout

---
+++ tests/test_put/test_e2e/run_trash_put/put_result.py
@@ -0,0 +1,37 @@
+from typing import NamedTuple
+
+from tests.run_command import CmdResult
+from tests.support.help_reformatting import reformat_help_message
+from tests.support.my_path import MyPath
+from tests.test_put.test_e2e.run_trash_put.stream import Stream
+
+
+class PutResult(NamedTuple('Output', [
+    ('stderr', Stream),
+    ('stdout', Stream),
+    ('exit_code', int),
+    ('temp_dir', MyPath),
+])):
+    def help_message(self):
+        return reformat_help_message(self.stdout.stream)
+
+    def combined(self):
+        return [self.stderr.cleaned() +
+                self.stdout.cleaned(),
+                self.exit_code]
+
+    def status(self):
+        return ["output is %s" % self.both().describe_stream(),
+                "exit code is %s" % self.exit_code]
+
+    def both(self):
+        return Stream(stream=self.stderr.stream + self.stdout.stream,
+                      temp_dir=self.temp_dir)
+
+def make_put_result(result,  # type: CmdResult
+                    temp_dir,  # type: MyPath
+                    ):  # type: (...) -> PutResult
+    return PutResult(stdout=Stream(stream=result.stdout, temp_dir=temp_dir),
+                     stderr=Stream(stream=result.stderr, temp_dir=temp_dir),
+                     exit_code=result.exit_code,
+                     temp_dir=temp_dir)
--- tests/test_put/test_e2e/test_on_links_to_dirs.py
+++ tests/test_put/test_e2e/test_on_links_to_dirs.py
@@ -1,47 +1,48 @@
-import os
-
 import pytest
 
 from tests.run_command import temp_dir  # noqa
-from tests.test_put.test_e2e.run_trash_put import run_trash_put
+from tests.test_put.test_e2e.run_trash_put.directory_layout import \
+    DirectoriesLayout
 from trashcli.put.fs.real_fs import RealFs
-
-fs = RealFs()
 
 
 @pytest.mark.slow
 class TestOnLinksToDirs:
     def test_link_to_dir_without_slashes(self, temp_dir):
-        fs.mkdir(temp_dir / 'a-dir')
-        fs.touch(temp_dir / "a-file")
+        layout = DirectoriesLayout(temp_dir, RealFs())
+        layout.make_cur_dir()
+        layout.mkdir_in_cur_dir('a-dir')
+        layout.touch_in_cur_dir('a-file')
+        layout.symlink_in_cur_dir('a-dir', 'link-to-dir')
 
-        fs.symlink(temp_dir / 'a-dir', temp_dir / 'link-to-dir')
+        result = layout.run_trash_put(['link-to-dir'])
 
-        output = run_trash_put(temp_dir, ['link-to-dir'])
-
-        assert output.stderr.lines() == [
-            "trash-put: 'link-to-dir' trashed in /trash-dir"
-        ]
-        assert fs.list_sorted(temp_dir) == ['a-dir', 'a-file', 'trash-dir']
+        assert result.status() == {
+            'command output': "trash-put: 'link-to-dir' trashed in /trash-dir",
+            'file left in current_dir': ['/a-dir', '/a-file'],
+            'file in trash dir': ['/files',
+                                  '/files/link-to-dir',
+                                  '/info',
+                                  '/info/link-to-dir.trashinfo'],
+        }
 
     def test_link_to_dir_with_slashes(self, temp_dir):
-        fs.mkdir(temp_dir / 'a-dir')
-        fs.touch(temp_dir / "a-file")
+        layout = DirectoriesLayout(temp_dir, RealFs())
+        layout.make_cur_dir()
+        layout.mkdir_in_cur_dir("a-dir")
+        layout.touch_in_cur_dir("a-file")
+        layout.symlink_in_cur_dir('a-dir', 'link-to-dir')
 
-        fs.symlink(temp_dir / 'a-dir', temp_dir / 'link-to-dir')
+        result = layout.run_trash_put(['link-to-dir/'])
 
-        output = run_trash_put(temp_dir, ['link-to-dir/'])
+        assert result.status() == {
+            'command output': "trash-put: 'link-to-dir/' trashed in /trash-dir",
+            'file left in current_dir': ['/a-file', '/link-to-dir'],
+            'file in trash dir': ['/files',
+                                  '/files/link-to-dir',
+                                  '/info',
+                                  '/info/link-to-dir.trashinfo'],
+        }
 
-        assert temp_dir.list_all_files_sorted() == [
-            '/a-file',
-            '/link-to-dir',
-            '/trash-dir',
-            '/trash-dir/files',
-            '/trash-dir/files/link-to-dir',
-            '/trash-dir/info',
-            '/trash-dir/info/link-to-dir.trashinfo']
-        assert output.stderr.lines() == [
-            "trash-put: 'link-to-dir/' trashed in /trash-dir"]
-        assert fs.list_sorted(temp_dir) == ['a-file',
-                                            'link-to-dir',
-                                            'trash-dir']
+
+
