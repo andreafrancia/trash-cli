e4bcc6b544642f0ac55d31c5afe2e877bdf4ca81
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2024-03-28 11:50:30 +0100

    Fix tests non passing in linux. Made them working with linux and mac.

--- tests/test_put/test_stat_mode.py
+++ tests/test_put/test_stat_mode.py
@@ -1,14 +1,19 @@
+import os
+
 from tests.run_command import temp_dir  # noqa
 from trashcli.put.fs.real_fs import RealFs
 from trashcli.put.octal import octal
 
-
 class TestStatMode:
     def setup_method(self):
         self.fs = RealFs()
+        self.old_umask = os.umask(0o777 - 0o755)
+
+    def teardown_method(self):
+        os.umask(self.old_umask)
 
     def test_mode_for_a_dir(self, temp_dir):
-        self.fs.mkdir(temp_dir / 'foo')
+        self.fs.mkdir_with_mode(temp_dir / 'foo', 0o755)
         stat = self.fs.lstat(temp_dir / 'foo')
         assert octal(stat.mode) == '0o40755'
 
@@ -18,6 +23,7 @@ class TestStatMode:
         assert octal(stat.mode) == '0o100644'
 
     def test_mode_for_a_symlink(self, temp_dir):
+        os.umask(0o777 - 0o777)
         self.fs.symlink(temp_dir / 'foo', temp_dir / 'bar')
         stat = self.fs.lstat(temp_dir / 'bar')
-        assert octal(stat.mode) == '0o120755'
+        assert octal(stat.mode) == '0o120777'
