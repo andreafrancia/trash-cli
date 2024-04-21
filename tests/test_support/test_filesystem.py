b054e284fe4ec3a719c0cc0e328aaa235dee6b82
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2024-04-21 22:59:47 +0200

    Refactor: reorganizing files in under tests/

--- tests/test_support/test_filesystem.py
+++ tests/test_support/test_filesystem.py
@@ -6,8 +6,8 @@ import unittest
 import pytest
 
 from trashcli.fs import has_sticky_bit, mkdirs, is_sticky_dir
-from .support.files import make_empty_file, set_sticky_bit, unset_sticky_bit
-from .support.my_path import MyPath
+from tests.support.files import make_empty_file, set_sticky_bit, unset_sticky_bit
+from tests.support.my_path import MyPath
 
 
 @pytest.mark.slow
