4e35fe3546a227b1ecac0b33457ccd00c384cd98
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2024-04-21 20:28:57 +0200

    Refactor: simplified TestFileSize test

--- tests/test_list/components/test_file_size.py
+++ tests/test_list/components/test_file_size.py
@@ -1,19 +1,11 @@
-import unittest
-
+from tests.run_command import temp_dir  # qa
 from tests.support.files import make_file
-from tests.support.my_path import MyPath
 
 from trashcli import fs
 
 
-class Test_file_size(unittest.TestCase):
-    def setUp(self):
-        self.tmp_dir = MyPath.make_temp_dir()
-
-    def test(self):
-        make_file(self.tmp_dir / 'a-file', '123')
-        result = fs.file_size(self.tmp_dir / 'a-file')
+class TestFileSize:
+    def test(self, temp_dir):
+        make_file(temp_dir / 'a-file', '123')
+        result = fs.file_size(temp_dir / 'a-file')
         assert 3 == result
-
-    def tearDown(self):
-        self.tmp_dir.clean_up()
