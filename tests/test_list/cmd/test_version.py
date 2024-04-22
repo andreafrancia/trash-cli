be2fc50908df05f525b9719bcf56ead67d7c7689
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2024-04-22 18:54:33 +0200

    Refactor: using trash_list_user fixture in TestVersion

--- tests/test_list/cmd/test_version.py
+++ tests/test_list/cmd/test_version.py
@@ -1,18 +1,16 @@
 # Copyright (C) 2011-2024 Andrea Francia Trivolzio(PV) Italy
 
 from tests.support.asserts import assert_equals_with_unidiff
-from tests.support.my_path import MyPath
-from tests.test_list.cmd.support.trash_list_user import TrashListUser
+from tests.test_list.cmd.support.trash_list_user import trash_list_user
+
+user = trash_list_user
 
 
 class TestVersion:
-    def setup_method(self):
-        self.user = TrashListUser(MyPath.make_temp_dir())
+    def test_should_output_the_version(self, user):
+        user.set_version('1.2.3')
 
-    def test_should_output_the_version(self):
-        self.user.set_version('1.2.3')
-
-        output = self.user.run_trash_list('--version')
+        output = user.run_trash_list('--version')
 
         assert_equals_with_unidiff('trash-list 1.2.3\n',
                                    output.whole_output())
