andreafrancia/trash-cli

commit 006774ec31e4859c0b71f5e019e877ac5a4e6a63

Improved error message when TrashInfo is not parsable in trash-restore

trashcli/restore/main.py
@@ -4,7 +4,8 @@ import sys
 
 import trashcli.trash
 from .file_system import RealRestoreReadFileSystem, \
-    RealRestoreWriteFileSystem, RealReadCwd, RealFileReader, ListingFileSystem
+    RealRestoreWriteFileSystem, RealReadCwd, RealFileReader, \
+    RealListingFileSystem
 from .info_dir_searcher import InfoDirSearcher
 from .info_files import InfoFiles
 from .restore_cmd import RestoreCmd
@@ -12,14 +13,13 @@ from .trash_directories import TrashDirectoriesImpl
 from .trashed_file import TrashedFiles
 from ..fstab.volumes import RealVolumes
 from ..lib.logger import my_logger
-from ..lib.my_input import my_input
-from ..list_mount_points import RealMountPointsListing
+from ..lib.my_input import MyInput
 
 
 def main():
-    info_files = InfoFiles(ListingFileSystem())
-    trash_directories = TrashDirectoriesImpl(RealMountPointsListing(),
-                                             RealVolumes(),
+    info_files = InfoFiles(RealListingFileSystem())
+    volumes = RealVolumes()
+    trash_directories = TrashDirectoriesImpl(volumes,
                                              os.getuid(),
                                              os.environ)
     searcher = InfoDirSearcher(trash_directories, info_files)
@@ -30,7 +30,7 @@ def main():
         stdout=sys.stdout,
         stderr=sys.stderr,
         exit=sys.exit,
-        input=my_input,
+        input=MyInput(),
         version=trashcli.trash.version,
         trashed_files=trashed_files,
         read_fs=RealRestoreReadFileSystem(),

tests/test_put/support/fake_fs/fake_fs.py
@@ -2,14 +2,15 @@ import os
 
 from tests.test_put.support.fake_fs.directory import Directory, \
     make_inode_for_dir
-from tests.test_put.support.fake_fs.inode import SymLink
 from tests.test_put.support.fake_fs.file import File
+from tests.test_put.support.fake_fs.inode import SymLink
 from tests.test_put.support.format_mode import format_mode
 from tests.test_put.support.my_file_not_found_error import MyFileNotFoundError
+from trashcli.fs import PathExists
 from trashcli.put.fs.fs import Fs
 
 
-class FakeFs(Fs):
+class FakeFs(Fs, PathExists):
     def __init__(self, cwd='/'):
         directory = Directory('/')
         make_inode_for_dir(directory, 0o755)

trashcli/empty/main.py
@@ -12,7 +12,7 @@ from .file_system_dir_reader import FileSystemDirReader
 from .top_trash_dir_rules_file_system_reader import \
     TopTrashDirRulesFileSystemReader
 from ..fstab.volume_listing import RealVolumesListing
-from ..fstab.volumes import RealVolumes
+from ..fstab.volume_of import RealVolumeOf
 
 
 def main():
@@ -26,7 +26,7 @@ def main():
                          content_reader=FileSystemContentReader(),
                          dir_reader=FileSystemDirReader(),
                          version=trash.version,
-                         volumes=RealVolumes())
+                         volumes=RealVolumeOf())
     return empty_cmd.run_cmd(sys.argv[1:], os.environ, os.getuid())
 
 

trashcli/list/main.py
@@ -5,8 +5,8 @@ import sys
 import trashcli.trash
 from trashcli.file_system_reader import FileSystemReader
 from trashcli.fstab.volume_listing import RealVolumesListing
-from trashcli.fstab.volumes import RealVolumes
-from trashcli.fstab.volumes import Volumes
+from trashcli.fstab.volume_of import RealVolumeOf
+from trashcli.fstab.volume_of import VolumeOf
 from trashcli.lib.print_version import PrintVersionArgs, \
     PrintVersionAction
 from trashcli.list.list_trash_action import ListTrashAction, ListTrashArgs
@@ -29,7 +29,7 @@ def main():
         environ=os.environ,
         volumes_listing=RealVolumesListing(),
         uid=os.getuid(),
-        volumes=RealVolumes(),
+        volumes=RealVolumeOf(),
         file_reader=FileSystemReader(),
         version=trashcli.trash.version
     ).run(sys.argv)
@@ -42,7 +42,7 @@ class ListCmd:
                  environ,
                  volumes_listing,
                  uid,
-                 volumes,  # type: Volumes
+                 volumes,  # type: VolumeOf
                  file_reader,
                  version,
                  ):

trashcli/put/main.py
@@ -2,10 +2,8 @@ import os
 import random
 import sys
 
-import trashcli.fstab
-import trashcli.fstab.volumes
-import trashcli.trash
-from trashcli.lib.my_input import my_input
+from trashcli.fstab.volume_of import RealVolumeOf
+from trashcli.lib.my_input import MyInput
 from trashcli.put.clock import RealClock
 from trashcli.put.describer import Describer
 from trashcli.put.dir_maker import DirMaker
@@ -35,12 +33,17 @@ from trashcli.put.user import User
 
 def main():
     cmd = make_cmd(clock=RealClock(), fs=RealFs(),
-                   my_input=my_input, randint=random.randint,
-                   stderr=sys.stderr, volumes=trashcli.fstab.volumes.RealVolumes())
+                   my_input=MyInput(), randint=random.randint,
+                   stderr=sys.stderr, volumes=RealVolumeOf())
     return cmd.run(sys.argv, os.environ, os.getuid())
 
 
-def make_cmd(clock, fs, my_input, randint, stderr, volumes):
+def make_cmd(clock,
+             fs,
+             my_input, # type: MyInput
+             randint,
+             stderr,
+             volumes):
     logger = MyLogger(stderr)
     describer = Describer(fs)
     reporter = TrashPutReporter(logger, describer)

commit 1375b1a0398920971a92b4ec39f28f10d1103b5d

Fixed regression that would crash trash-restore on non parseable trashinfo, with an error like: "TypeError: not enough arguments for format string"
The regression was introduced in commit 006774ec31e4859c0b71f5e019e877ac5a4e6a63 (Improved error message when TrashInfo is not parsable in trash-restore Andrea Francia 04/05/23, 18:23)

trashcli/restore/main.py
@@ -8,6 +8,7 @@ from .file_system import RealRestoreReadFileSystem, \
     RealListingFileSystem
 from .info_dir_searcher import InfoDirSearcher
 from .info_files import InfoFiles
+from .real_restore_logger import RealRestoreLogger
 from .restore_cmd import RestoreCmd
 from .trash_directories import TrashDirectoriesImpl
 from .trashed_files import TrashedFiles
@@ -23,7 +24,7 @@ def main():
                                              os.getuid(),
                                              os.environ)
     searcher = InfoDirSearcher(trash_directories, info_files)
-    trashed_files = TrashedFiles(my_logger,
+    trashed_files = TrashedFiles(RealRestoreLogger(my_logger),
                                  RealFileReader(),
                                  searcher)
     RestoreCmd.make(

trashcli/restore/real_restore_logger.py
@@ -1,0 +1,13 @@
+from logging import Logger
+
+from trashcli.restore.restore_logger import RestoreLogger
+
+
+class RealRestoreLogger(RestoreLogger):
+    def __init__(self,
+                 logger,  # type: Logger
+                 ):
+        self._logger = logger
+
+    def warning(self, message):
+        self._logger.warning(message)

trashcli/restore/restore_logger.py
@@ -1,0 +1,6 @@
+from trashcli.compat import Protocol
+
+
+class RestoreLogger(Protocol):
+    def warning(self, message):
+        raise NotImplementedError

trashcli/restore/trashed_files.py
@@ -1,5 +1,7 @@
-from logging import Logger
-from typing import Optional, Iterable, NamedTuple, Union
+from typing import Iterable
+from typing import NamedTuple
+from typing import Optional
+from typing import Union
 
 from trashcli.lib.path_of_backup_copy import path_of_backup_copy
 from trashcli.parse_trashinfo.parse_deletion_date import parse_deletion_date
@@ -7,12 +9,13 @@ from trashcli.parse_trashinfo.parse_original_location import \
     parse_original_location
 from trashcli.restore.file_system import FileReader
 from trashcli.restore.info_dir_searcher import InfoDirSearcher
+from trashcli.restore.restore_logger import RestoreLogger
 from trashcli.restore.trashed_file import TrashedFile
 
 
 class TrashedFiles:
     def __init__(self,
-                 logger,  # type: Logger
+                 logger,  # type: RestoreLogger
                  file_reader,  # type: FileReader
                  searcher,  # type: InfoDirSearcher
                  ):
@@ -29,7 +32,7 @@ class TrashedFiles:
             elif type(event) is NonParsableTrashInfo:
                 self.logger.warning(
                     "Non parsable trashinfo file: %s, because %s" %
-                    event.path, event.reason)
+                    (event.path, event.reason))
             elif type(event) is IOErrorReadingTrashInfo:
                 self.logger.warning(str(event))
             elif type(event) is TrashedFileFound:

tests/support/put/fake_fs/fake_fs.py
@@ -1,6 +1,8 @@
 import errno
 import os
 
+from typing import cast
+
 from tests.support.fakes.fake_volume_of import FakeVolumeOf
 from tests.support.put.fake_fs.directory import Directory
 from tests.support.put.fake_fs.directory import make_inode_dir
@@ -58,6 +60,9 @@ class FakeFs(FakeVolumeOf, Fs, PathExists):
         directory = self.get_entity_at(dirname)
         directory.add_dir(basename, 0o755, path)
 
+    def mkdir_p(self, path):
+        self.makedirs(path, 0o755)
+
     def get_entity_at(self, path):  # type: (str) -> Ent
         inode = check_cast(INode, self.get_entry_at(path))
         return inode.entity
@@ -104,8 +109,10 @@ class FakeFs(FakeVolumeOf, Fs, PathExists):
         if isinstance(entry, SymLink):
             link_target = self.readlink(path)
             return self.read(os.path.join(dirname, link_target))
+        elif isinstance(as_inode(entry).entity, File):
+            return cast(File, as_inode(entry).entity).content
         else:
-            return as_inode(entry).reg_file().content
+            raise IOError("Unable to read: %s" % path)
 
     def readlink(self, path):
         path = self._join_cwd(path)

tests/support/put/fake_fs/inode.py
@@ -28,7 +28,3 @@ class INode:
     def directory(self):
         from tests.support.put.fake_fs.directory import Directory
         return check_cast(Directory, self.entity)
-
-    def reg_file(self):
-        from tests.support.put.fake_fs.file import File
-        return check_cast(File, self.entity)

tests/test_restore/components/trashed_files/test_trashed_files.py
@@ -1,34 +1,95 @@
 import datetime
 import unittest
 
-from mock import Mock
+from six import StringIO
 
-from trashcli.restore.file_system import FakeFileReader
-from trashcli.restore.info_dir_searcher import InfoDirSearcher, FileFound
+from tests.support.put.fake_fs.fake_fs import FakeFs
+from tests.test_restore.support.fake_logger import FakeLogger
+from tests.test_restore.support.restore_fake_fs import RestoreFakeFs
+from trashcli.restore.info_dir_searcher import InfoDirSearcher
+from trashcli.restore.info_files import InfoFiles
+from trashcli.restore.trash_directories import TrashDirectories
+from trashcli.restore.trashed_file import TrashedFile
 from trashcli.restore.trashed_files import TrashedFiles
 
 
 class TestTrashedFiles(unittest.TestCase):
     def setUp(self):
-        self.file_reader = FakeFileReader()
-        self.logger = Mock(spec=[])
-        self.searcher = Mock(spec=InfoDirSearcher)
+        self.fs = FakeFs()
+        self.out = StringIO()
+        self.logger = FakeLogger(self.out)
+
+        class FakeTrashDirectories(TrashDirectories):
+            def list_trash_dirs(self, trash_dir_from_cli):
+                return [('/trash-dir', '/')]
+
+        self.searcher = InfoDirSearcher(FakeTrashDirectories(),
+                                        InfoFiles(RestoreFakeFs(self.fs)))
         self.trashed_files = TrashedFiles(self.logger,
-                                          self.file_reader,
+                                          RestoreFakeFs(self.fs),
                                           self.searcher)
+        self.fs.mkdir_p("/trash-dir/info")
 
     def test(self):
-        self.searcher.all_file_in_info_dir.return_value = [
-            FileFound('trashinfo', 'info/info_path.trashinfo', '/volume')
-        ]
-        self.file_reader.set_content(
-            'Path=name\nDeletionDate=2001-01-01T10:10:10')
+        self.fs.write_file('/trash-dir/info/info_path.trashinfo',
+                           'Path=name\n'
+                           'DeletionDate=2001-01-01T10:10:10')
 
         trashed_files = list(self.trashed_files.all_trashed_files(None))
 
-        trashed_file = trashed_files[0]
-        assert '/volume/name' == trashed_file.original_location
-        assert (datetime.datetime(2001, 1, 1, 10, 10, 10) ==
-                trashed_file.deletion_date)
-        assert 'info/info_path.trashinfo' == trashed_file.info_file
-        assert 'files/info_path' == trashed_file.original_file
+        assert {
+                   'trashed_files': trashed_files,
+                   'out': self.out.getvalue()} == {
+                   'trashed_files': [
+                       TrashedFile('/name',
+                                   datetime.datetime(2001, 1, 1,
+                                                     10, 10,
+                                                     10),
+                                   '/trash-dir/info/info_path.trashinfo',
+                                   '/trash-dir/files/info_path'),
+                   ],
+                   'out': ''
+               }
+
+    def test_on_non_trashinfo(self):
+        self.fs.touch('/trash-dir/info/info_path.non-trashinfo')
+
+        trashed_files = list(self.trashed_files.all_trashed_files(None))
+
+        assert {
+                   'trashed_files': trashed_files,
+                   'out': self.out.getvalue()} == {
+                   'trashed_files': [],
+                   'out': 'WARN: Non .trashinfo file in info dir\n'
+               }
+
+    def test_on_non_parsable_trashinfo(self):
+        self.fs.write_file('/trash-dir/info/info_path.trashinfo',
+                           '')
+
+        trashed_files = list(self.trashed_files.all_trashed_files(None))
+
+        assert {
+                   'trashed_files': trashed_files,
+                   'out': self.out.getvalue()} == {
+                   'trashed_files': [],
+                   'out': 'WARN: Non parsable trashinfo file: '
+                          '/trash-dir/info/info_path.trashinfo, because '
+                          'Unable to parse Path\n'
+               }
+
+    def test_on_io_error(self):
+        self.fs.mkdir_p('/trash-dir/info/info_path.trashinfo')
+
+        trashed_files = list(self.trashed_files.all_trashed_files(None))
+
+        assert {
+                   'trashed_files': trashed_files,
+                   'out': self.out.getvalue()
+               } == {
+                   'trashed_files': [],
+                   'out': "WARN: IOErrorReadingTrashInfo("
+                          "path='/trash-dir/info/info_path.trashinfo', "
+                          "error='Unable to read: "
+                          "/trash-dir/info/info_path.trashinfo')\n"
+               }

tests/test_restore/support/fake_logger.py
@@ -1,0 +1,10 @@
+from __future__ import print_function
+from trashcli.restore.trashed_files import RestoreLogger
+
+
+class FakeLogger(RestoreLogger):
+    def __init__(self, out):
+        self.out = out
+
+    def warning(self, message):
+        print("WARN: %s" % message, file=self.out)

tests/test_restore/support/restore_fake_fs.py
@@ -1,0 +1,21 @@
+import os
+
+from typing import Iterable
+
+from trashcli.restore.file_system import FileReader
+from trashcli.restore.file_system import ListingFileSystem
+
+
+class RestoreFakeFs(FileReader, ListingFileSystem):
+    def __init__(self,
+                 fs,  # type FakeFs
+                 ):
+        self.fs = fs
+
+    def contents_of(self, path):
+        return self.fs.read(path)
+
+    def list_files_in_dir(self, path):  # type: (str) -> Iterable[str]
+        for entry in self.fs.listdir(path):
+            result = os.path.join(path, entry)
+            yield result