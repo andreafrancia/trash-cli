9ea4cca19e13f9c531999e6cd2ad27438f4a9b0c
abstract-official <ywx20220878@outlook.com>
2026-04-17 23:43:20 +0800

    Fix: fix fake attrib access generated by AI
    This is what happens if you trust AI.

--- trashcli/rm/prepare_output_message.py
+++ trashcli/rm/prepare_output_message.py
@@ -3,4 +3,4 @@ def prepare_output_message(info_files):
         return 'No files to be removed.'
     else:
         return 'The following files will be removed:\n' + '\n'.join(
-            '    - ' + info_file.original_location for info_file in info_files) + '\nProceed? (y/N) '
+            '    - ' + info_file for info_file in info_files) + '\nProceed? (y/N) '
