562ddaec0d0f69cf9eaed90fab2c83a3d0240890
curious-rabbit <curious-rabbit@local>
2026-07-08 11:19:42 +0200

    better fix

--- tests/test_put/cmd/test_put_orphaned_copy.py
+++ tests/test_put/cmd/test_put_orphaned_copy.py
@@ -1,28 +1,30 @@
-from tests.support.put.fake_fs.failing_fake_fs import FailingFakeFs
+from six import StringIO
+
+from tests.support.put.fake_fs.fake_fs import FakeFs
 from tests.support.put.fake_random import FakeRandomInt
 from tests.test_put.support.recording_backend import RecordingBackend
-from six import StringIO
 from tests.support.put.dummy_clock import FixedClock, jan_1st_2024
 from trashcli.lib.my_input import HardCodedInput
 from trashcli.put.main import make_cmd
 
 
-class TestPutOrphanedCopy:
+class TestPutRefusesUndeletableFile:
     def setup_method(self):
-        self.fs = FailingFakeFs()
+        self.fs = FakeFs()
         self.user_input = HardCodedInput('y')
         self.randint = FakeRandomInt([])
 
-    def test_a_file_that_cannot_be_removed_is_not_left_as_a_copy(self):
-        self.fs.touch("/foo")
-        # the move copies the file into the trash then fails to delete the original
-        self.fs.fail_move_leaving_copy_on("/foo")
+    def test_a_file_that_cannot_be_deleted_is_not_trashed_or_copied(self):
+        self.fs.makedirs('/readonly', 0o755)
+        self.fs.make_file('/readonly/foo', '')
+        # the parent is not writable, so foo cannot be deleted by the user
+        self.fs.chmod('/readonly', 0o500)
 
-        self.run_cmd(['trash-put', '/foo'])
+        self.run_cmd(['trash-put', '/readonly/foo'])
 
-        assert self.fs.exists("/foo")
-        assert self.fs.ls_aa('/.Trash-123/files') == []
-        assert self.fs.ls_aa('/.Trash-123/info') == []
+        assert self.fs.exists('/readonly/foo')
+        assert not self.fs.exists('/.Trash-123/files/foo')
+        assert not self.fs.exists('/.Trash-123/info/foo.trashinfo')
 
     def run_cmd(self, args):
         cmd = make_cmd(clock=FixedClock(jan_1st_2024()), fs=self.fs,
