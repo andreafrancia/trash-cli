c1f7f7a4c1e1cadef6cfe3c5cdfdf3eae427e1ec
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2026-07-12 22:45:35 +0200

    Fix Self in my_path.py

--- tests/support/dirs/my_path.py
+++ tests/support/dirs/my_path.py
@@ -1,43 +1,46 @@
 import os
 import shutil
 import tempfile
-from typing import Any, Generator, Self
+from typing import Any, Generator, TypeVar
 
 from six import StringIO
 
 from trashcli.put.fs.fs import list_all
 from trashcli.put.fs.real_fs import RealFs
 
+Self = TypeVar('Self', bound='MyPath')
+
 
 class MyPath(str):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,  # type: Self
+                 *args, **kwargs):
         super(MyPath, self).__init__()
         self.fs = RealFs()
 
-    def __truediv__(self,
+    def __truediv__(self,  # type: Self
                     other_path,
                     ):  # type: (...) -> MyPath
         return self.path_join(other_path)
 
-    def clean_str(self,   # type: MyPath
+    def clean_str(self,  # type: Self
                   stdout,  # type: StringIO
-                  ):   # type: (...) -> str
+                  ):  # type: (...) -> str
         return stdout.getvalue().replace(self, '')
 
-    def read(self,
+    def read(self,  # type: Self
              path):
         return self.fs.read(self / path)
 
-    def __div__(self,
+    def __div__(self,  # type: Self
                 other_path):
         return self.path_join(other_path)
 
-    def path_join(self,
+    def path_join(self,  # type: Self
                   other_path):
         return MyPath(os.path.join(self, other_path))
 
-    def existence_of(self,
+    def existence_of(self,  # type: Self
                      *paths):
         return [self.existence_of_single(p) for p in paths]
 
@@ -58,7 +61,7 @@ class MyPath(str):
         full_path = self.join_no_slash(path)
         return os.path.exists(full_path)
 
-    def join_no_slash(self,
+    def join_no_slash(self,  # type: Self
                       path,  # type: str
                       ):
         no_slash = self._no_slash(path)
@@ -70,12 +73,14 @@ class MyPath(str):
             path = path[1:]
         return path
 
-    def find_files_rel(self):
+    def find_files_rel(self,  # type: Self
+                       ):
         for path in self.find_all():
             if path.is_file():
                 yield self.rel_path(path)
 
-    def find_all(self):  # type: (...) -> Generator[MyPath, Any, None]
+    def find_all(self,  # type: Self
+                 ):  # type: (...) -> Generator[MyPath, Any, None]
         for path in list_all(self.fs, self):
             yield MyPath(path)
 
@@ -107,19 +112,20 @@ class MyPath(str):
                     src, dest):
         self.fs.symlink(self / src, self / dest)
 
-    def mkdir_rel(self,
+    def mkdir_rel(self,  # type: Self
                   path,
                   ):
         self.fs.mkdir(self / path)
         return self / path
 
-    def mkdir_rel_p(self,
+    def mkdir_rel_p(self,  # type: Self
                     path,
                     ):
         self.fs.mkdirs(self / path)
         return self / path
 
-    def mkdirs(self):
+    def mkdirs(self,  # type: Self
+               ):
         self.fs.mkdirs(self)
         return self
 
@@ -132,13 +138,14 @@ class MyPath(str):
                ):  # type: (...) -> MyPath
         return MyPath(os.path.dirname(self))
 
-    def clean_up(self):
+    def clean_up(self,  # type: Self
+                 ):
         shutil.rmtree(self)
 
     @classmethod
     def make_temp_dir(
             cls
-                      ):  # type: (...) -> Self
+    ):  # type: (...) -> Self
         temp_from_os = tempfile.mkdtemp(suffix="_trash_cli_test")
         realpath_just_to_be_sure = os.path.realpath(temp_from_os)
         return cls(realpath_just_to_be_sure)
